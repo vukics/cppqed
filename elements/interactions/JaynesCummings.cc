93db57775cc12b82a1d12646726c523191e8d0a9
András Vukics <vukics@users.sourceforge.net>
2011-07-01 14:14:16 +0200

    JaynesCummings migrated to new architecture

--- elements/interactions/JaynesCummings.cc
+++ elements/interactions/JaynesCummings.cc
@@ -18,7 +18,7 @@ Pars::Pars(parameters::ParameterTable& p, const std::string& mod)
 
 Base::Base(const QbitBase* qbit, const ModeBase* mode, const dcomp& g)
   : IA_Base(Frees(qbit,mode),RealFreqs(),tuple_list_of("g",g,sqrt(mode->getDimension()))),
-    TDH_Base(tridiagMinusHC(conj(g)*qbit::sigmaop()*aop(mode).dagger()),freqs(qbit)*freqs(mode))
+    TDH_Base(tridiagMinusHC(conj(g)*qbit::sigmaop(qbit)*aop(mode).dagger()))
 {
   getParsStream()<<"# Jaynes-Cummings interaction\n";
 }
