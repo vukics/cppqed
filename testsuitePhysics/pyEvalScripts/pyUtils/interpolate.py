1e961b35a3e7a0c82bd8bdee64eb805d089ff2bb
András Vukics <vukics@users.sourceforge.net>
2011-08-05 16:07:46 +0200

    testsuite restructuring close-to-ideal state achieved

--- testsuitePhysics/pyEvalScripts/pyUtils/interpolate.py
+++ testsuitePhysics/pyEvalScripts/pyUtils/interpolate.py
@@ -3,7 +3,7 @@
 import scipy.interpolate
 
 def interpolate(timeArray,array) :
-    return scipy.interpolate.interp1d(timeArray,array)
+    return scipy.interpolate.interp1d(timeArray,array) # scipy.interpolate.interp1d(timeArray,array,kind='cubic') takes immense amount of time
 
 def interpolateEVC(evc,i) :
     return interpolate(evc.time,evc[i]) 
