e2d55ae5b56d318ca6e352f9a6d894c9fbe94bf7
András Vukics <vukics@users.sourceforge.net>
2011-10-11 20:33:47 +0200

    small

--- testsuite/TridiagonalBenchmarkCompilation.cc
+++ testsuite/TridiagonalBenchmarkCompilation.cc
@@ -1,9 +1,9 @@
 #include "Tridiagonal.h"
 
-#define RANK 5
+#define RANK 7
 
 
-typedef typename quantumoperator::Tridiagonal<RANK>::StateVectorLow StateVectorLow;
+typedef quantumoperator::Tridiagonal<RANK>::StateVectorLow StateVectorLow;
 
 template void quantumoperator::Tridiagonal<RANK>::apply(const StateVectorLow& psi, StateVectorLow& dpsidt) const;
 
