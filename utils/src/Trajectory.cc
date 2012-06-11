ebb19f4ac28cd77934726a536c5ad4a8b073948c
Raimar Sandner <raimar.sandner@uibk.ac.at>
2012-06-11 15:39:27 +0200

    Introduced new --NDt flag for NDt mode of operation.
    Use NDt mode *only* if dc=0, Dt> and NDt>0. T is ignored in this case. Instead advance the trajectory for exactly NDt display steps, i.e. end time is NDt*Dt for new trajectories and t0+NDt*Dt for continued trajectories.
    
    Implementation notes: trajectory::details::run has new template parameter typename L for the limit, as this now can be long in Ndt mode or double in all other cases. The work is delegated to a new function details::doRunNDt.

--- utils/src/Trajectory.cc
+++ utils/src/Trajectory.cc
@@ -74,6 +74,12 @@ void details::doRun(TrajectoryBase& traj, double time, double deltaT)
   }
 }
 
-
+void details::doRunNDt(TrajectoryBase& traj, long nDt, double deltaT)
+{
+  for (long i=0; i<nDt; i++){
+    traj.evolve(deltaT);
+    traj.display();
+  }
+}
 
 } // trajectory
