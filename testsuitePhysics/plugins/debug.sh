3bd90a6807e99bbad92104a5a635312774c7b3af
András Vukics <vukics@users.sourceforge.net>
2012-10-03 10:30:58 +0200

    Factoring out testsuitePhysics into a separate branch.

--- testsuitePhysics/plugins/debug.sh
+++
@@ -1,1 +0,0 @@
-echo "Output: $1 Command: $2"
--- testsuitePhysics/plugins/pbs.sh
+++
@@ -1,9 +0,0 @@
-echo "#!/bin/bash" > temp.sh
-echo "#" >> temp.sh
-echo "#PBS -N $1" >> temp.sh
-#echo "#PBS -l nodes=1,cput=10:00:00" >> temp.sh
-echo "#PBS -q eternity -e errorfiles" >> temp.sh
-
-echo "$2 --o $1" >> temp.sh
-qsub -o /dev/null temp.sh
-rm temp.sh
