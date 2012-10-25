2d7e0f0e0832287fcdf2c399182494368dc9d236
vukics <vukics@users.sourceforge.net>
2012-10-25 15:46:44 +0200

    Modified JaynesCummings to accept Spins besides Qbits—compile-time polymorphy-based solution.

--- interactions/JaynesCummings.cc
+++ interactions/JaynesCummings.cc
@@ -1,17 +1,7 @@
 #include "JaynesCummings_.h"
 
-#include "Qbit_.h"
-#include "Spin.h"
-
-#include "impl/Tridiagonal.tcc"
-
 #include "impl/Pars.tcc"
 
-#include <boost/assign/list_of.hpp>
-
-using namespace boost::assign; using boost::dynamic_pointer_cast;
-
-using namespace mode;
 
 namespace jaynescummings {
 
@@ -21,20 +11,11 @@ Pars::Pars(parameters::ParameterTable& p, const std::string& mod)
 {}
 
 
-Base::Base(QbitSpinCommonPtr spin, mode::Ptr mode, const dcomp& g)
-  : IA_Base(Frees(spin,mode),RealFreqs(),tuple_list_of("g",g,sqrt(spin->getDimension()*mode->getDimension()))),
-    TDH_Base(tridiagMinusHC(conj(g)*sigmaop(spin)*aop(mode).dagger()))
-{
-  getParsStream()<<"# Jaynes-Cummings interaction\n";
-}
+const structure::free::Tridiagonal sigmaop(qbit::Ptr qbit) {return qbit::sigmaop(qbit);}
 
 
-const structure::free::Tridiagonal sigmaop(QbitSpinCommonPtr qbitspin)
-{
-  if      (const qbit::Ptr qbit=dynamic_pointer_cast<const QbitBase>(qbitspin)) return sigmaop(qbit);
-  else if (const spin::Ptr spin=dynamic_pointer_cast<const SpinBase>(qbitspin)) return spin::sminus (spin);
-  else throw UnrecognizedPluginToJaynesCummings();
-}
+const structure::free::Tridiagonal sigmaop(spin::Ptr spin) {return spin::sminus(spin);}
+
 
 } // jaynescummings
 
