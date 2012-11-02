87b3dbfee41cddcbb16715ba441f1658b3347924
András Vukics <vukics@users.sourceforge.net>
2012-11-02 17:55:11 +0100

    Demonstrating a new way of recycling code.

--- elements/composites/BinarySystem.cc
+++ elements/composites/BinarySystem.cc
@@ -1,3 +1,5 @@
+#ifndef BINARY_SYSTEM_AVERAGED_LIOUVILLEAN_ITERATING
+
 #include "BinarySystem.h"
 
 #include "Interaction.h"
@@ -14,40 +16,6 @@
 
 
 using composite::SubSystemFree;
-
-
-#define DISPLAY_KEY(Class,Aux) void binary::Class::displayKey_v(std::ostream& os, size_t& i) const \
-  {									\
-    os<<"# Binary system\n";						\
-    free0_.display##Aux##Key(os,i);					\
-    free1_.display##Aux##Key(os,i);					\
-    ia_   .display##Aux##Key(os,i);					\
-  }									\
-
-
-#define ADD_UP_N(Class,Func) size_t binary::Class::n##Func##_v() const	\
-  {									\
-    return free0_.n##Func() + free1_.n##Func() + ia_.n##Func(); \
-  }									\
-
-
-#define CONCATENATE_ARRAYS(Class,ArrayName,func,NumberName) const binary::Class::ArrayName binary::Class::func##_v(double t, const LazyDensityOperator& ldo) const \
-  {									\
-  using quantumdata::partialTrace;					\
-  using boost::copy;							\
-									\
-  const ArrayName							\
-    a0 (partialTrace<V0,ArrayName>(ldo,bind(&SubSystemFree::func,free0_,t,_1))), \
-    a1 (partialTrace<V1,ArrayName>(ldo,bind(&SubSystemFree::func,free1_,t,_1))), \
-    a01(ia_.func(t,ldo));						\
-									\
-  ArrayName a(n##NumberName());						\
-									\
-  copy(a01,copy(a1,copy(a0,a.begin())));				\
-									\
-  return a;								\
-  }									\
-
 
 
 using namespace structure;
@@ -94,55 +62,75 @@ void binary::Base::displayParameters_v(std::ostream& os) const
 }
 
 
-DISPLAY_KEY(Base,Averaged);
 
-ADD_UP_N(Base,Avr);
-
-CONCATENATE_ARRAYS(Base,Averages,average,Avr);
+#define SUCCESSIVE_Ranges(f0,f1,ia) ptrdiff_t l=-1, u;  \
+  if ((u=l+free0_.nAvr())>l) {                          \
+    PROCESS_Range( averages(blitz::Range(l+1,u)) , f0)  \
+  }                                                     \
+  if ((l=u+free1_.nAvr())>u) {                          \
+    PROCESS_Range( averages(blitz::Range(u+1,l)) , f1)  \
+  }                                                     \
+  if ((u=l+ia_.nAvr())>l) {                             \
+    PROCESS_Range( averages(blitz::Range(l+1,u)) , ia)  \
+  }                                                     \
 
 
 void binary::Base::process_v(Averages& averages) const
 {
-  using blitz::Range;
-
-  ptrdiff_t l=-1, u;
-
-  if ((u=l+free0_.nAvr())>l) {
-    Averages temp(averages(Range(l+1,u)));
-    free0_.process(temp);
-  }
-  if ((l=u+free1_.nAvr())>u) {
-    Averages temp(averages(Range(u+1,l)));
-    free1_.process(temp);
-  }
-  if ((u=l+ia_.nAvr())>l) {
-    Averages temp(averages(Range(l+1,u)));
-    ia_.process(temp);
-  }
-
+#define PROCESS_Range(av,ss) Averages temp(av); ss.process(temp);
+  
+  SUCCESSIVE_Ranges(free0_,free1_,ia_) ;
+  
+#undef  PROCESS_Range
+  
 }
-
 
 
 void binary::Base::display_v(const Averages& averages, std::ostream& os, int precision) const
 {
-  using blitz::Range;
-
   const Av1::Ptr 
     av0 =free0_.getAv(),
     av1 =free1_.getAv();
   const Av2::Ptr
     av01=   ia_.getAv();
 
-  ptrdiff_t l=-1, u;
-
-  if ((u=l+free0_.nAvr())>l) av0 ->display(averages(Range(l+1,u)),os,precision);
-
-  if ((l=u+free1_.nAvr())>u) av1 ->display(averages(Range(u+1,l)),os,precision);
-
-  if ((u=l+   ia_.nAvr())>l) av01->display(averages(Range(l+1,u)),os,precision);
+#define PROCESS_Range(av,ss) ss->display(av,os,precision);
+  
+  SUCCESSIVE_Ranges(av0,av1,av01) ;
+  
+#undef  PROCESS_Range
 
 }
+
+#undef  SUCCESSIVE_Ranges
+
+////////////////////////////
+//                        //
+// Averaged - Liouvillean //
+//                        //
+////////////////////////////
+
+
+#define BINARY_SYSTEM_AVERAGED_LIOUVILLEAN_ITERATING
+
+#define Class      Base
+#define keyName    displayAveragedKey
+#define numberName nAvr
+#define ArrayName  Averages
+#define func       average
+
+#include "BinarySystem.cc"
+
+
+#define BINARY_SYSTEM_AVERAGED_LIOUVILLEAN_ITERATING
+
+#define Class      Liouvillean
+#define keyName    displayLiouvilleanKey
+#define numberName nJumps
+#define ArrayName  Probabilities
+#define func       probabilities
+
+#include "BinarySystem.cc"
 
 
 
@@ -199,11 +187,6 @@ void binary::Hamiltonian::addContribution_v(double t, const StateVectorLow& psi,
 /////////////////
 
 
-ADD_UP_N(Liouvillean,Jumps)
-
-CONCATENATE_ARRAYS(Liouvillean,Probabilities,probabilities,Jumps);
-
-
 void binary::Liouvillean::actWithJ_v(double t, StateVectorLow& psi, size_t i) const
 {
   using namespace blitzplusplus::basi;
@@ -231,9 +214,6 @@ void binary::Liouvillean::actWithJ_v(double t, StateVectorLow& psi, size_t i) co
 }
 
 
-DISPLAY_KEY(Liouvillean,Liouvillean)
-
-
 
 //////////////////
 //              //
@@ -256,9 +236,6 @@ BinarySystem<IS_EX,IS_HA,IS_LI>::BinarySystem(Interaction::Ptr ia)
 
 
 #undef BASE_CTOR
-#undef CONCATENATE_ARRAYS
-#undef ADD_UP_N
-#undef DISPLAY_KEY
 
 
 namespace {
@@ -307,3 +284,50 @@ template class BinarySystem<false,true ,true >;
 template class BinarySystem<false,true ,false>;
 template class BinarySystem<false,false,true >;
 template class BinarySystem<false,false,false>;
+
+
+#else // BINARY_SYSTEM_AVERAGED_LIOUVILLEAN_ITERATING
+
+
+void binary::Class::displayKey_v(std::ostream& os, size_t& i) const
+{
+  os<<"# Binary system\n";
+  free0_.keyName(os,i);
+  free1_.keyName(os,i);
+  ia_   .keyName(os,i);
+}
+
+
+size_t binary::Class::BOOST_PP_CAT(numberName,_v)() const
+{
+  return free0_.numberName() + free1_.numberName() + ia_.numberName();
+}
+
+
+const binary::Class::ArrayName binary::Class::BOOST_PP_CAT(func,_v)(double t, const LazyDensityOperator& ldo) const
+{
+  using quantumdata::partialTrace;
+  using boost::copy;
+
+  const ArrayName
+    a0 (partialTrace<V0,ArrayName>(ldo,bind(&SubSystemFree::func,free0_,t,_1))),
+    a1 (partialTrace<V1,ArrayName>(ldo,bind(&SubSystemFree::func,free1_,t,_1))),
+    a01(ia_.func(t,ldo));
+
+  ArrayName a(numberName());
+
+  copy(a01,copy(a1,copy(a0,a.begin())));
+
+  return a;
+}
+
+#undef Class
+#undef keyName
+#undef numberName
+#undef ArrayName
+#undef func
+
+#undef BINARY_SYSTEM_AVERAGED_LIOUVILLEAN_ITERATING
+
+#endif // BINARY_SYSTEM_AVERAGED_LIOUVILLEAN_ITERATING
+
