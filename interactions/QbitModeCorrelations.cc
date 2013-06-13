a39e7bac139ce1cc1758d9d7402dcefefbdd1f2d
vukics <vukics@users.sourceforge.net>
2013-06-13 12:53:34 +0200

    Introduced DimensionsBookkeeper::getDimension.

--- interactions/QbitModeCorrelations.cc
+++ interactions/QbitModeCorrelations.cc
@@ -22,7 +22,7 @@ QbitModeCorrelations::average_v(const LazyDensityOperator& matrix) const
   Averages averages(2);
   averages=0;
 
-  for (int n=0; n<int(matrix.getDimensions()[1])-1; n++) {
+  for (int n=0; n<int(matrix.getDimension(1))-1; n++) {
     dcomp temp=sqrt(n+1)*matrix(Idx(1,n),Idx(0,n+1));
     averages(0)+=real(temp);
     averages(1)+=imag(temp);
