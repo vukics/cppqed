53bb7bc8591ba589101c4e6d1a57ba81a5c5e3bb
András Vukics <vukics@users.sourceforge.net>
2013-07-08 14:31:55 +0200

    Added CompilationMacros.h

---
+++ CompilationMacros.h
@@ -0,0 +1,4 @@
+// -*- C++ -*-
+/// \briefFile{Collecting the macros for compilation configuration to one place for documentation with doxygen. This file is never included in the framework.}
+
+#define DO_CONSIDER_EXPLICITLY_SPECIALIZED_TRIDIAGONAL_APPLIES 1 ///< Governs which implementation of quantumoperator::Tridiagonal::apply is considered.
