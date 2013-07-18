35681383f829a0465cb060bceebe99b7fc9a50b5
András Vukics <vukics@users.sourceforge.net>
2013-07-18 23:00:14 +0200

    quantumoperator namespace finished

--- quantumoperator/Sigma.h
+++ quantumoperator/Sigma.h
@@ -1,4 +1,5 @@
 // -*- C++ -*-
+/// \briefFileDefault
 #ifndef   QUANTUMOPERATOR_SIGMA_H_INCLUDED
 #define   QUANTUMOPERATOR_SIGMA_H_INCLUDED
 
@@ -9,7 +10,13 @@
 
 namespace quantumoperator {
 
-
+/**
+ * \defgroup sigmadirectproducts Direct-product operators for Sigma
+ * 
+ * These operators aptly demonstrate the use of DirectProduct and the meaning of its template parameters.
+ * 
+ * @{
+ */
 template<int L, int R, typename OTHER>
 const DirectProduct<L,R,OTHER,true >
 operator*(const Sigma<L,R>&, const OTHER&);
@@ -22,7 +29,15 @@ template<int L1, int R1, int L2, int R2>
 const DirectProduct<L1,R1,Sigma<L2,R2>,true>
 operator*(const Sigma<L1,R1>&, const Sigma<L2,R2>&);
 
+//@}
 
+
+/// Stateless class implementing the unary quantumoperator \f$\ket{L}\bra{R}\f$.
+/**
+ * The template parameters are eventually converted to runtime data when the Sigma::apply function is called. At that point, dimensionality errors are detected in debug mode.
+ * 
+ * \note Sigma should perhaps inherit from DimensionsBookkeeper as this would allow for an earlier detection of dimensionality errors.
+ */
 template<int L, int R>
 class Sigma
 {
@@ -31,22 +46,34 @@ public:
 
   typedef quantumdata::Types<1>::StateVectorLow StateVectorLow;
 
+  /// Application of the operator on a state vector can be implemented trivially
   void
   apply(const StateVectorLow& psi, StateVectorLow& dpsidt) const
   {
     dpsidt(L)+=psi(R);
   }  
 
+  /// Hermitian conjugation also trivial
   const Sigma<R,L> dagger() const {return Sigma<R,L>();}
 
 };
 
 
+/// Helper for DirectProduct::apply
+/**
+ * Calculates the state-vector slice \f[\ket{\Psi^{\avr{1,2,3,…,\text{rank-2},\text{rank-1}}}(\iota_0=n)}\in\bigotimes_{i=1,2,3,…,\text{rank-2},\text{rank-1}}\HSpace_i\f]
+ * if `IS_HEAD=true` and \f[\ket{\Psi^{\avr{0,1,2,…,\text{rank-3},\text{rank-2}}}(\iota_\text{rank-1}=n)}\in\bigotimes_{i=0,1,2,…,\text{rank-3},\text{rank-2}}\HSpace_i\f]
+ * if `IS_HEAD=false`. The code is automatically generated for all template-parameter combinations (`RANK` up to #BLITZ_ARRAY_LARGEST_RANK) via preprocessor metaprogramming.
+ * Cf. quantumoperator/Sigma.cc
+ * ~~~{.sh}
+ * g++ -P -E -Iutils/include/ -Iquantumoperator/ -Iquantumdata/ quantumoperator/Sigma.cc | tail -n128
+ * ~~~
+ * 
+ * \note It’s better to convert `n` into a runtime variable because then we can use complete specializations of this function. Eventually it has to be converted anyway into an index of `psi`.
+ */
 template<int RANK, bool IS_HEAD>
 const typename quantumdata::Types<RANK-1>::StateVectorLow
 partialProject(const typename quantumdata::Types<RANK>::StateVectorLow& psi, int n);
-// It's better to convert n into a runtime variable because then we can use complete specializations of this function
-// Eventually it has to be converted anyway into an index of psi
 
 } // quantumoperator
 
