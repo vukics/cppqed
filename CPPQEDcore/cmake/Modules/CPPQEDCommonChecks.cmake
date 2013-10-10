e5fda63f978d07254defa59cc5c5d0ae04d6ce2c
Raimar Sandner <raimar.sandner@uibk.ac.at>
2013-10-10 13:09:35 +0200

    cmake cleanup

--- CPPQEDcore/cmake/Modules/CPPQEDCommonChecks.cmake
+++ CPPQEDcore/cmake/Modules/CPPQEDCommonChecks.cmake
@@ -1,16 +1,3 @@
-if(Boost_FOUND)
-  if(Boost_MAJOR_VERSION EQUAL "1" AND Boost_MINOR_VERSION GREATER "51")
-    add_definitions(-DBOOST_RESULT_OF_USE_TR1)
-    message(STATUS "added -DBOOST_RESULT_OF_USE_TR1" )
-  endif()
-endif()
-
-if(CBLAS_FOUND AND "${CBLAS_LIBRARIES}" MATCHES "gslcblas")
-  add_definitions(-DGSL_CBLAS)
-  message(STATUS "added -DGSL_CBLAS" )
-endif(CBLAS_FOUND AND "${CBLAS_LIBRARIES}" MATCHES "gslcblas")
-
-
 MACRO(CPPQED_CXX_FLAGS)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -Wno-sign-compare")
   set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DBZ_DEBUG -Wall -Wextra -Wpointer-arith -Wcast-qual -Wcast-align -Wwrite-strings -Wno-deprecated -Wnon-virtual-dtor -Wno-ignored-qualifiers -Wno-sign-compare")
