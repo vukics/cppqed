04abd0907f76956146d87394ffb095ee7ac0feae
András Vukics <vukics@users.sourceforge.net>
2013-11-19 15:34:42 +0100

    Corrected bug in QbitModeCorrelations

--- interactions/QbitModeCorrelations.cc
+++ interactions/QbitModeCorrelations.cc
@@ -4,7 +4,7 @@
 
 
 QbitModeCorrelations::QbitModeCorrelations()
-  : EA_Base("QbitModeCorrelations",{"real(<sigma*adagger>","imag(\")"})
+  : EA_Base("QbitModeCorrelations",{"real(<sigma*adagger>","imag(\")","real(<sigma*a>)","imag(\")","real(<sigmaZ*a>)","imag(\")"})
 {
 }
 
@@ -22,12 +22,12 @@ QbitModeCorrelations::average_v(NoTime, const LazyDensityOperator& matrix) const
     averages(0)+=real(temp);
     averages(1)+=imag(temp);
   }
-  for (int n=0; n<int(matrix.getDimensions()[1])-1; n++) {
+  for (int n=0; n<int(matrix.getDimension(1))-1; n++) {
     dcomp temp=sqrt(n+1)*matrix(Idx(1,n+1),Idx(0,n));
     averages(2)+=real(temp);
     averages(3)+=imag(temp);
   }
-  for (int n=0; n<int(matrix.getDimensions()[1])-1; n++) {
+  for (int n=0; n<int(matrix.getDimension(1))-1; n++) {
     dcomp temp=.5*sqrt(n+1)*(matrix(Idx(1,n+1),Idx(1,n))-matrix(Idx(0,n+1),Idx(0,n)));
     averages(4)+=real(temp);
     averages(5)+=imag(temp);
