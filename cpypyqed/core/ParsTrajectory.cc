22c77faca2f9ccd6321e3e04b0897cb0c18d8c8f
Raimar Sandner <raimar.sandner@uibk.ac.at>
2013-12-23 18:51:27 +0100

    update ParsTrajectory to reflect the new layout ParsRun and ParsEvolved

--- cpypyqed/core/ParsTrajectory.cc
+++ cpypyqed/core/ParsTrajectory.cc
@@ -1,5 +1,6 @@
 // -*- C++ -*-
 #include "PythonExtension.h"
+#include "Namespaces.h"
 
 #include "EvolvedGSL.h"
 #include "FormDouble.h"
@@ -16,48 +17,62 @@ using std::string;
 
 namespace pythonext{
 
-PARS_GETTER_SETTER(double, ParsRun, T)
-PARS_GETTER_SETTER(int,    ParsRun, dc)
-PARS_GETTER_SETTER(double, ParsRun, Dt)
-PARS_GETTER_SETTER(long,   ParsRun, NDt)
-PARS_GETTER_SETTER(string, ParsRun, ofn)
+PARS_GETTER_SETTER(double,  ParsRun, T)
+PARS_GETTER_SETTER(int,     ParsRun, dc)
+PARS_GETTER_SETTER(double,  ParsRun, Dt)
+PARS_GETTER_SETTER(long,    ParsRun, NDt)
+PARS_GETTER_SETTER(string,  ParsRun, ofn)
+PARS_GETTER_SETTER(string,  ParsRun, initialFileName)
+PARS_GETTER_SETTER(int,     ParsRun, precision)
+PARS_GETTER_SETTER(bool,    ParsRun, displayInfo)
+PARS_GETTER_SETTER(unsigned,ParsRun, sdf)
+
 
 PARS_GETTER_SETTER(double, ParsEvolved, epsRel)
 PARS_GETTER_SETTER(double, ParsEvolved, epsAbs)
-
-
-
-
-PARS_GETTER_SETTER(int,    Pars, precision)
-PARS_GETTER_SETTER(bool,   Pars, displayInfo)
-PARS_GETTER_SETTER(evolved::SteppingFunction, Pars, sf)
-PARS_GETTER_SETTER(double, Pars, nextDtTryCorrectionFactor)
+PARS_GETTER_SETTER(evolved::SteppingFunction, ParsEvolved, sf)
+PARS_GETTER_SETTER(double, ParsEvolved, nextDtTryCorrectionFactor)
 
 void export_ParsTrajectory()
 {
-  enum_<evolved::SteppingFunction>("SteppingFunction")
-    .value("SF_RK8PD",  evolved::SF_RK8PD)
-    .value("SF_RKCK",   evolved::SF_RKCK)
-  ;
-  class_<Pars>
-    (
-      "ParsTrajectory",
-      init<parameters::ParameterTable&, optional<const std::string&> >()
-        [with_custodian_and_ward<1,2>()]
-    )
-    .PARS_PROPERTY(T)
-    .PARS_PROPERTY(epsRel)
-    .PARS_PROPERTY(epsAbs)
-    .PARS_PROPERTY(dc)
-    .PARS_PROPERTY(Dt)
-    .PARS_PROPERTY(NDt)
-    .PARS_PROPERTY(ofn)
-    .PARS_PROPERTY(autoStop)
-    .PARS_PROPERTY(precision)
-    .PARS_PROPERTY(displayInfo)
-    .PARS_PROPERTY(sf)
-    .PARS_PROPERTY(nextDtTryCorrectionFactor)
-  ;
+  {
+    scope namespaceScope = evolvedNameSpace;
+    enum_<evolved::SteppingFunction>("SteppingFunction")
+      .value("SF_RK8PD",  evolved::SF_RK8PD)
+      .value("SF_RKCK",   evolved::SF_RKCK)
+    ;
+  }
+
+  {
+    scope namespaceScope = trajectoryNameSpace;
+    class_<ParsRun>
+      (
+        "ParsRun",
+        init<parameters::ParameterTable&, optional<const string&> >()
+          [with_custodian_and_ward<1,2>()]
+      )
+      .PARS_PROPERTY(T)
+      .PARS_PROPERTY(dc)
+      .PARS_PROPERTY(Dt)
+      .PARS_PROPERTY(NDt)
+      .PARS_PROPERTY(ofn)
+      .PARS_PROPERTY(initialFileName)
+      .PARS_PROPERTY(precision)
+      .PARS_PROPERTY(displayInfo)
+      .PARS_PROPERTY(sdf)
+    ;
+    class_<ParsEvolved>
+      (
+        "ParsEvolved",
+        init<parameters::ParameterTable&, optional<const string&> >()
+          [with_custodian_and_ward<1,2>()]
+      )
+      .PARS_PROPERTY(epsRel)
+      .PARS_PROPERTY(epsAbs)
+      .PARS_PROPERTY(sf)
+      .PARS_PROPERTY(nextDtTryCorrectionFactor)
+    ;
+  }
 }
 
 }
