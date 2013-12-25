4f4421f3bb759dee754341bdf1fd176e78db865f
Raimar Sandner <raimar.sandner@uibk.ac.at>
2013-12-25 13:47:52 +0100

    QbitMode script

---
+++ cpypyqed/scripts/QbitMode.py
@@ -0,0 +1,21 @@
+#!/bin/env python
+
+from cpypyqed import *
+import sys
+
+p = parameters.ParameterTable()
+pm=mode.ParsPumpedLossy(p)
+pq=qbit.ParsPumpedLossy(p)
+pe=ParsEvolution(p)
+pjc=jaynescummings.Pars(p)
+
+parameters.update(p,sys.argv,'--')
+
+m=mode.make(pm,QMP.IP)
+q=qbit.make(pq,QMP.IP)
+jc=jaynescummings.make(q,m,pjc)
+
+psi = (qbit.init(pq)*mode.init(pm))
+psi.renorm()
+
+evolve(psi, binary.make(jc), pe)
