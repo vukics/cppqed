bda94c3453fcfcdca77ac1ab6710b5f7a0c96d08
Raimar Sandner <raimar.sandner@uibk.ac.at>
2013-12-31 06:14:05 +0100

    registered Interaction::Ptr to python, implicit conversion to const pointers

--- cpypyqed/core/Interaction.cc
+++ cpypyqed/core/Interaction.cc
@@ -15,6 +15,10 @@ void export_Interaction() {
   scope namespaceScope = structureNameSpace;
   class_<Interaction<2>, boost::noncopyable >("Interaction2", no_init);
   class_<Interaction<3>, boost::noncopyable >("Interaction3", no_init);
+  register_ptr_to_python<Interaction<2>::Ptr>();
+  implicitly_convertible<boost::shared_ptr<Interaction<2>>, Interaction<2>::Ptr>();
+  register_ptr_to_python<Interaction<3>::Ptr>();
+  implicitly_convertible<boost::shared_ptr<Interaction<3>>, Interaction<3>::Ptr>();
 }
 
 
