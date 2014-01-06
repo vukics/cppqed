3d81bea68e780ba19cb50ccc973992bca9bcd3ff
András Vukics <vukics@users.sourceforge.net>
2014-01-06 12:41:45 +0100

    ParsTrajectory

--- CPPQEDcore/utils/ParsTrajectory.h
+++ CPPQEDcore/utils/ParsTrajectory.h
@@ -1,3 +1,4 @@
+/// \briefFile{Defines parameter aggergates to Trajectory.h}
 // -*- C++ -*-
 #ifndef UTILS_PARSTRAJECTORY_H_INCLUDED
 #define UTILS_PARSTRAJECTORY_H_INCLUDED
@@ -11,50 +12,61 @@
 
 namespace trajectory {
 
-/*
-  The following suit of Pars(Stochastic)Trajectory, + eg ParsMCWF in C++QED demonstrates a technique to facilitate reading parameters from command line and using them to initialize a (hierarchy of) class(es).
-
-  The same technique is to be used in quantum/elements when there is a detailed hierarchy of elements.
-
-  In principle the use of Pars(...) classes could be avoided by initializing classes with a ParameterTable. Eg
-
-  class X {
-  double& y1;
-  double& y2;
-
-  public:
-  X(Pars::ParameterTable& p) : 
-  y1(p.add("y1","y1 description",y1default)), y2(p.add("y2","y2 description",y2default)) {}
-
-  };
-
-  Avoiding this technique is a design decision: the drawback is that too many things (virtually everything) would depend on Pars, and every parameter inside classes has to be declared as reference, which then makes it difficult to initialize the class NOT with a ParameterTable.
-
-*/
-
+/// Aggregate condensing parameters concerning adaptive ODE evolution (cf. Adaptive::Adaptive()) in the style of a parameters::ParameterTable
+/**
+ * The suit of ParsEvolved, ParsRun, ParsStochastic demonstrates a technique to facilitate reading parameters from command line and using them to initialize a (hierarchy of) class(es).
+ * 
+ * The same technique is to be used in elements when there is a detailed hierarchy of elements.
+ * 
+ * In principle the use of such `%Pars…` classes could be avoided by initializing classes with a parameters::ParameterTable. E.g.
+ * 
+ *     class X {
+ * 
+ *       double& y1;
+ *       double& y2;
+ * 
+ *     public:
+ *       X(Pars::ParameterTable& p) : y1(p.add("y1","y1 description",y1default)), y2(p.add("y2","y2 description",y2default)) {}
+ * 
+ *     };
+ * 
+ * Avoiding this technique is a design decision: its drawback would be that too many things (virtually everything) would depend on the \link parameters parameter-bundle\endlink,
+ * and every parameter inside classes has to be declared as reference, which then makes it difficult to initialize the class in any other way than with a parameters::ParameterTable.
+ *
+ */
 struct ParsEvolved
 {
-  static const double epsRelDefault;
-  static const double epsAbsDefault;
+  static const double epsRelDefault; ///< The ultimate default of \link ParsEvolved::epsRel epsRel\endlink in the framework
+  static const double epsAbsDefault; ///< ” for \link PrasEvolved::epsAbs epsAbs\endlink
 
-  double &epsRel, &epsAbs;
+  double
+    &epsRel, ///< relative precision of ODE stepping (cf. evolved::TimeStepBookkeeper)
+    &epsAbs; ///< absolute precision ”
 
-  evolved::SteppingFunction& sf;
-  double &nextDtTryCorrectionFactor;
+  evolved::SteppingFunction& sf; ///< \link evolved::SteppingFunction stepping-function type\endlink
+  double &nextDtTryCorrectionFactor; ///< cf. evolved::MakerGSL::MakerGSL()
   
-  ParsEvolved(parameters::ParameterTable&, const std::string& mod="");
+  /// All `%Pars…` classes are constructed taking a parameters::ParameterTable, to register the parameters on
+  /** 
+   * This occurs via the parameters::ParameterTable::addMod member function returning a reference to the registered parameter wherewith the public attributes
+   * (like ParsEvolved::epsRel) get initialized.
+   */
+  ParsEvolved(parameters::ParameterTable&, ///<[in/out] the table to register the new parameters on
+              const std::string& mod=""    ///<[in] possible modifier suffix
+             );
   
 };
 
 
+/// Parameters corresponding to the different versions of run()
+/** \see ParsEvolved for general explanation of the workings of `%Pars…` classes */
 struct ParsRun
 {
-  double &T;
+  double &T; ///< endtime of the run
   int &dc;
   double &Dt;
-  long &NDt;
-  std::string &ofn;
-  std::string &initialFileName;
+  long &NDt; ///< number of deltaT intervals in \link run(Trajectory&, long, double, unsigned, const std::string&, const std::string&, int, bool, bool) deltaT-mode\endlink
+  std::string &ofn, &initialFileName;
 
   formdouble::Zero &precision;
 
@@ -67,6 +79,7 @@ struct ParsRun
 };
 
 
+/// An example of a simple unification of `%Pars…` classes, here ParsRun & ParsEvolved
 struct Pars : ParsRun, ParsEvolved
 {
   Pars(parameters::ParameterTable& p, const std::string& mod="") : ParsRun(p,mod), ParsEvolved(p,mod) {}
