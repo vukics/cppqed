b772ae1be0ffe23312bd30ebbde8fc88e6dd5184
András Vukics <vukics@users.sourceforge.net>
2014-02-17 15:47:23 +0100

    Corrected compilefail test

--- Testing/compilefail/CompositeError2.cc
+++ Testing/compilefail/CompositeError2.cc
@@ -2,4 +2,4 @@
 
 // Does not compile: (NEGATIVE_ELEMENT_in_NONNEGATIVE_VECTOR)
 
-const int v=composite::result_of::Make<Act<1,0>,Act<2,0>,Act<3,4>,Act<1,2,0>,Act<2,-1,0>,Act<3,2,0,1> >::type::RANK ; 
+template struct composite::result_of::Make<Act<1,0>,Act<2,0>,Act<3,4>,Act<1,2,0>,Act<2,-1,0>,Act<3,2,0,1> >;
