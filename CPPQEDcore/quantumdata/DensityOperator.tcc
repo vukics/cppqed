vukics/cppqed

commit ebd12f98b6f7cd562495068fdf0be10b8a0f0555

Addresses issue #22 by removing spurious using declarations… … in case of referencing names in templatized base classes, replacing them with this-> specifications

CPPQEDelements/utils/AveragingUtils.h
@@ -30,8 +30,6 @@ public:
   
   ReducedDensityOperator(const std::string&, const Dimensions&, bool offDiagonals=false, const KeyLabels& =KeyLabels());
 
-  using DimensionsBookkeeper<RANK>::getDimensions; using DimensionsBookkeeper<RANK>::getTotalDimension; using Base::nAvr;
-  
 private:
   static const KeyLabels helper(const Dimensions&, bool offDiagonals, const KeyLabels&);
 
@@ -54,8 +52,6 @@ public:
   typedef typename Base::Dimensions Dimensions;
   typedef typename Base::Averages Averages;
   typedef typename Base::LazyDensityOperator LazyDensityOperator;
-  
-  using Base::getDimensions; using Base::getTotalDimension;
   
   ReducedDensityOperatorNegativity(const std::string& label, const Dimensions& dim)
     : Base(label,dim,true,boost::assign::list_of("negativity")) {}
@@ -92,8 +88,6 @@ public:
 
 private:
   Base*const do_clone() const {return new Collecting(*this);}
-
-  using Base::nAvr; using Base::getLabels;
 
   const Averages average_v(Time, const LazyDensityOperator&) const;
 

CPPQEDelements/utils/AveragingUtils.tcc
@@ -65,7 +65,7 @@ ReducedDensityOperator<RANK>::helper(const Dimensions& dim, bool offDiagonals, c
 template<int RANK>
 ReducedDensityOperator<RANK>::ReducedDensityOperator(const std::string& label, const Dimensions& dim, bool offDiagonals, const KeyLabels& subsequent) :
   DimensionsBookkeeper<RANK>(dim),
-  Base(label,helper(getDimensions(),offDiagonals,subsequent)),
+  Base(label,helper(this->getDimensions(),offDiagonals,subsequent)),
   offDiagonals_(offDiagonals)
 {
 }
@@ -92,9 +92,9 @@ ReducedDensityOperatorNegativity<RANK,V>::average_v(structure::NoTime t, const L
 template<int RANK, typename V>
 void ReducedDensityOperatorNegativity<RANK,V>::process_v(Averages& averages) const
 {
-  quantumdata::DensityOperator<RANK> rho(getDimensions());
+  quantumdata::DensityOperator<RANK> rho(this->getDimensions());
   linalg::CMatrix matrix(rho.matrixView()); // references the same data
-  const size_t dim=getTotalDimension();
+  const size_t dim=this->getTotalDimension();
   
   {
     int idx=0;  
@@ -134,7 +134,7 @@ template<int RANK, bool IS_TIME_DEPENDENT>
 auto
 averagingUtils::Collecting<RANK,IS_TIME_DEPENDENT>::average_v(Time t, const LazyDensityOperator& matrix) const -> const Averages
 {
-  Averages res(nAvr()); res=0;
+  Averages res(this->nAvr()); res=0;
   return cpputils::concatenate( collection_ | boost::adaptors::transformed(bind(&Element::average,_1,details::convert(t),boost::cref(matrix))) , res);
 
 }

CPPQEDcore/doc/rationale.markdown
@@ -56,6 +56,8 @@ Implementation rationales {#implementationrationales}
 
 General robust style + Effective C++ + Exceptional C++
 
+* When refering to names in templatized base classes, prefer the `this->` style instead of the `using` style (cf. Item 43 in \cite meyers) – use the latter only when the name in question is needed many times (introduce the `using` declaration to the same access level as the original)
+
 * Be aware that \refStdCppConstruct{assert,cassert/assert} works only in debug mode (probably a macro switches it off in `NDEBUG`). It will not even evaluate its argument in `NDEBUG` mode!
 
 * Note that function template partial specializations are not allowed, but the same effect can almost always be achieved by function template *overloads*.

CPPQEDcore/quantumdata/DensityOperator.h
@@ -67,7 +67,7 @@ public:
 
   typedef linalg::CMatrix CMatrix;
 
-  using ABase::frobeniusNorm; using ABase::getArray; 
+  using ABase::frobeniusNorm; using ABase::getArray;
 
   /*
   DensityOperator() : Base() {}

CPPQEDcore/quantumdata/StateVector.h
@@ -65,7 +65,7 @@ public:
   typedef LazyDensityOperator<RANK> LDO_Base;
   typedef ArrayBase          <RANK>    ABase;
 
-  typedef typename LDO_Base::Dimensions         Dimensions        ;
+  typedef typename LDO_Base::Dimensions Dimensions;
 
   typedef typename ABase::ArrayLow StateVectorLow;
 
@@ -73,7 +73,7 @@ public:
 
   typedef typename LDO_Base::Idx Idx;
 
-  using LDO_Base::getTotalDimension; using ABase::vectorView; using ABase::getArray;
+  using ABase::vectorView; using ABase::getArray;
 
   /// \name Construction, assignment
   //@{

CPPQEDcore/quantumdata/StateVector.tcc
@@ -70,7 +70,7 @@ StateVector<RANK>::addTo(DensityOperator<RANK>& rho) const
 
   const CVector& vector(vectorView());
 
-  int dim(getTotalDimension());
+  int dim(this->getTotalDimension());
 
   for (int i=0; i<dim; i++) for (int j=0; j<dim; j++) matrix(i,j)+=vector(i)*conj(vector(j));
 

CPPQEDcore/quantumtrajectory/EnsembleMCWF.h
@@ -63,8 +63,6 @@ protected:
   const QuantumSystemPtr getQS() const {return qs_;}
 
 private:
-  using Ensemble::getTrajectories;
-  
   std::ostream& logOnEnd_v(std::ostream& os) const final;
   
   // static helpers to constructor
@@ -121,8 +119,6 @@ public:
   typedef typename Base      ::    StateVector     StateVector;
   typedef typename DO_Display::DensityOperator DensityOperator;
 
-  using Base::getQS; using Base::getTime; using Base::toBeAveraged;
-
   /// Templated constructor with the same idea as Master::Master
   /** \tparam SYS the physical system – can be any type convertible to structure::QuantumSystem::Ptr via cpputils::sharedPointerize */
   template<typename SYS>
@@ -133,10 +129,10 @@ public:
                bool negativity, ///< governs whether entanglement should be calculated, cf. display_densityoperator::_, quantumdata::negPT
                const StateVectorLow& scaleAbs=StateVectorLow() ///< has the same role as `scaleAbs` in evolved::Maker::operator()
                )
-    : Base(psi,cpputils::sharedPointerize(sys),p,scaleAbs), doDisplay_(structure::qsa<RANK>(getQS()),negativity) {}
+    : Base(psi,cpputils::sharedPointerize(sys),p,scaleAbs), doDisplay_(structure::qsa<RANK>(this->getQS()),negativity) {}
 
 private:
-  std::ostream& display_v   (std::ostream& os, int precision) const final {return doDisplay_.display   (getTime(),toBeAveraged(),os,precision);}
+  std::ostream& display_v   (std::ostream& os, int precision) const final {return doDisplay_.display   (this->getTime(),this->toBeAveraged(),os,precision);}
   std::ostream& displayKey_v(std::ostream& os, size_t& i    ) const final {return doDisplay_.displayKey(os,i);}
 
   const DO_Display doDisplay_;

CPPQEDcore/quantumtrajectory/EnsembleMCWF.tcc
@@ -56,8 +56,8 @@ std::ostream&
 quantumtrajectory::ensemble::Base<RANK>::logOnEnd_v(std::ostream& os) const
 {
   LoggerList loggerList;
-  for (typename Trajectories::const_iterator i=getTrajectories().begin(); i!=getTrajectories().end(); ++i)
-    if (const auto traj=dynamic_cast<const MCWF_Trajectory<RANK>*>(&(*i)))
+  for (auto& i : this->getTrajectories())
+    if (const auto traj=dynamic_cast<const MCWF_Trajectory<RANK>*>(&i))
       loggerList.push_back(traj->getLogger());
   
   return displayLog(os,loggerList,nBins_,nJumpsPerBin_);

commit caa78de3cf5a7bee4e8b00a3558064eac50d7bba

Introduced 2 convenience-overloads to MultiIndexIterator. + fixed small bug in AveragingUtils introduced in ebd12f98

CPPQEDcore/quantumdata/DensityOperator.tcc
@@ -90,7 +90,7 @@ void inflate(const DArray<1>& flattened, DensityOperator<RANK>& rho, bool offDia
   const size_t dim=rho.getTotalDimension();
   
   typedef cpputils::MultiIndexIterator<RANK> Iterator;
-  const Iterator etalon(typename DensityOperator<RANK>::Dimensions(size_t(0)),rho.getDimensions()-1,cpputils::mii::begin);
+  const Iterator etalon(rho.getDimensions()-1,cpputils::mii::begin);
   
   size_t idx=0;
 
@@ -122,7 +122,7 @@ densityOperatorize(const LazyDensityOperator<RANK>& matrix)
   DensityOperator<RANK> res(matrix.getDimension());
   
   typedef cpputils::MultiIndexIterator<RANK> Iterator;
-  const Iterator etalon(typename DensityOperator<RANK>::Dimensions(size_t(0)),matrix.getDimensions()-1,cpputils::mii::begin);
+  const Iterator etalon(matrix.getDimensions()-1,cpputils::mii::begin);
   
   typedef typename DensityOperator<RANK>::Idx Idx;
   

CPPQEDcore/quantumdata/LazyDensityOperator.tcc
@@ -50,7 +50,7 @@ const DArray<1> deflate(const LazyDensityOperator<RANK>& matrix, bool offDiagona
   DArray<1> res(offDiagonals ? sqr(dim) : dim);
   
   typedef cpputils::MultiIndexIterator<RANK> Iterator;
-  const Iterator etalon(Dimensions(size_t(0)),matrix.getDimensions()-1,cpputils::mii::begin);
+  const Iterator etalon(matrix.getDimensions()-1,cpputils::mii::begin);
   
   size_t idx=0;
 

CPPQEDcore/structure/Interaction.h
@@ -47,6 +47,7 @@ public:
   
   typedef typename DimensionsBookkeeper<RANK>::Dimensions Dimensions;
   
+  /// \todo Why not a variadic constructor here, taking a pack of Free instances?
   explicit Interaction(const Frees& frees,
                        const    RealFreqs&    realFreqs=emptyRF, 
                        const ComplexFreqs& complexFreqs=emptyCF)

CPPQEDcore/utils/MultiIndexIterator.h
@@ -54,10 +54,21 @@ public:
   MultiIndexIterator(const MultiIndex& lbound, ///< tiny vector comprising the sequence of lower bounds
                      const MultiIndex& ubound, ///< tiny vector comprising the sequence of upper bounds (inclusive!)
                      mii::Begin)
-    : lbound_(lbound), ubound_(ubound), idx_(lbound_) {} // if it's not the end, it's the beginning
+    : lbound_(lbound), ubound_(ubound), idx_(lbound_) {}
 
   MultiIndexIterator(const MultiIndex& lbound, const MultiIndex& ubound, mii::End  )
     : lbound_(lbound), ubound_(ubound), idx_(ubound_) {operator++();} ///< ” to the end of the sequence (which is in fact beyond the end by one)
+
+    /// \overload
+    /** `lbound` is all-zero */
+  MultiIndexIterator(const MultiIndex& ubound, ///< tiny vector comprising the sequence of upper bounds (inclusive!)
+                     mii::Begin b)
+    : MultiIndexIterator(MultiIndex(size_t(0)),ubound,b) {}
+
+    /// \overload
+    /** `lbound` is all-zero */
+  MultiIndexIterator(const MultiIndex& ubound, mii::End e)
+    : MultiIndexIterator(MultiIndex(size_t(0)),ubound,e) {}
   //@}
   
   MultiIndexIterator& operator=(const MultiIndexIterator& other) {idx_=other.idx_; return *this;}

CPPQEDelements/utils/AveragingUtils.h
@@ -28,6 +28,8 @@ public:
   
   typedef typename DimensionsBookkeeper<RANK>::Dimensions Dimensions;
   
+  using DimensionsBookkeeper<RANK>::getDimensions; using DimensionsBookkeeper<RANK>::getTotalDimension;
+
   ReducedDensityOperator(const std::string&, const Dimensions&, bool offDiagonals=false, const KeyLabels& =KeyLabels());
 
 private:
@@ -52,6 +54,8 @@ public:
   typedef typename Base::Dimensions Dimensions;
   typedef typename Base::Averages Averages;
   typedef typename Base::LazyDensityOperator LazyDensityOperator;
+  
+  using Base::getDimensions; using Base::getTotalDimension;
   
   ReducedDensityOperatorNegativity(const std::string& label, const Dimensions& dim)
     : Base(label,dim,true,boost::assign::list_of("negativity")) {}

CPPQEDelements/utils/AveragingUtils.tcc
@@ -29,7 +29,7 @@ ReducedDensityOperator<RANK>::helper(const Dimensions& dim, bool offDiagonals, c
   using std::stringstream;
 
   KeyLabels res;
-  Iterator i(Dimensions(size_t(0)),dim-1,cpputils::mii::begin);
+  Iterator i(dim-1,cpputils::mii::begin);
 
   // Diagonals
   for (; i!=i.getEnd(); ++i) {
@@ -65,7 +65,7 @@ ReducedDensityOperator<RANK>::helper(const Dimensions& dim, bool offDiagonals, c
 template<int RANK>
 ReducedDensityOperator<RANK>::ReducedDensityOperator(const std::string& label, const Dimensions& dim, bool offDiagonals, const KeyLabels& subsequent) :
   DimensionsBookkeeper<RANK>(dim),
-  Base(label,helper(this->getDimensions(),offDiagonals,subsequent)),
+  Base(label,helper(getDimensions(),offDiagonals,subsequent)),
   offDiagonals_(offDiagonals)
 {
 }
@@ -92,9 +92,9 @@ ReducedDensityOperatorNegativity<RANK,V>::average_v(structure::NoTime t, const L
 template<int RANK, typename V>
 void ReducedDensityOperatorNegativity<RANK,V>::process_v(Averages& averages) const
 {
-  quantumdata::DensityOperator<RANK> rho(this->getDimensions());
+  quantumdata::DensityOperator<RANK> rho(getDimensions());
   linalg::CMatrix matrix(rho.matrixView()); // references the same data
-  const size_t dim=this->getTotalDimension();
+  const size_t dim=getTotalDimension();
   
   {
     int idx=0;  