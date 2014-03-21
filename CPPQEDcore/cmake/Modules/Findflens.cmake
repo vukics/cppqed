30a1c309ad6a27f7134272704fdffda010e0baa2
András Vukics <vukics@users.sourceforge.net>
2014-03-21 20:37:12 +0100

    Adjusted build-system to the new header-only FLENS-LAPACK bundled build now uses our own SourceForge fork of FLENS

--- CPPQEDcore/cmake/Modules/Findflens.cmake
+++ CPPQEDcore/cmake/Modules/Findflens.cmake
@@ -6,10 +6,6 @@
 #!
 #! - `flens_FOUND` - system has flens
 #! - `flens_INCLUDE_DIRS` - the flens include directories
-#! - `flens_LIBRARIES` - link these to use flens
-
-include(LibFindMacros)
-libfind_package(flens CBLAS)
 
 if (UNIX)
   find_package(PkgConfig QUIET)
@@ -22,14 +18,7 @@ find_path(flens_INCLUDE_DIR
   PATHS ${flens_PKGCONF_INCLUDE_DIRS}
 )
 
-# Finally the library itself
-find_library(flens_LIBRARY
-  NAMES flens
-  PATHS ${flens_PKGCONF_LIBRARY_DIRS}
-)
-
 # Set the include dir variables and the libraries and let libfind_process do the rest.
 # NOTE: Singular variables for this library, plural for libraries this this lib depends on.
-set(flens_PROCESS_INCLUDES flens_INCLUDE_DIR CBLAS_INCLUDE_DIRS)
-set(flens_PROCESS_LIBS flens_LIBRARY CBLAS_LIBRARIES)
+set(flens_PROCESS_INCLUDES flens_INCLUDE_DIR)
 libfind_process(flens)
