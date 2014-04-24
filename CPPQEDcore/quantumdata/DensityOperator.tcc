vukics/cppqed

commit 6a25b988de265823ead4358b7f4e264859a40a23

Switching to new, shiny syntax for indexing LazyDensityOperators and DensityOperators – might break some unsupported elements

CPPQEDcore/quantumdata/DensityOperator.tcc
@@ -72,12 +72,11 @@ DensityOperator<RANK>::renorm()
 
 
 
-template<int RANK> 
+template<int RANK>
 const dcomp&
-DensityOperator<RANK>::operator()(const Idx& i, const Idx& j) const
+DensityOperator<RANK>::indexWithTiny(const Idx& i, const Idx& j) const
 {
-  return getArray()(blitzplusplus::concatenateTinies<int,int,RANK,RANK>(i,j));
-  // We have to explicitly indicate the template parameters for concatenateTinies here, otherwise when RANK=1, that is, Idx is int, the compiler cannot find the function. This is despite TinyVector<int,1> has an implicit constructor from int, because implicit type conversions are NEVER considered for template parameter deduction (for further details see EffC++ 3rd edition item 46.)
+  return getArray()(blitzplusplus::concatenateTinies(i,j));
 }
 
 
@@ -94,24 +93,18 @@ void inflate(const DArray<1>& flattened, DensityOperator<RANK>& rho, bool offDia
   
   size_t idx=0;
 
-  typedef typename DensityOperator<RANK>::Idx Idx;
   // Diagonal
-  for (Iterator i(etalon); idx<dim; ++i) {
-    const Idx ii(dispatchLDO_index(*i));
-    rho(ii,ii)=flattened(idx++);
-  }
+  for (Iterator i(etalon); idx<dim; ++i)
+    rho(*i)(*i)=flattened(idx++);
   
   // OffDiagonal
   if (offDiagonals)
-    for (Iterator i(etalon); idx<mathutils::sqr(dim); ++i) {
-      const Idx ii(dispatchLDO_index(*i));
+    for (Iterator i(etalon); idx<mathutils::sqr(dim); ++i)
       for (Iterator j=++Iterator(i); j!=etalon.getEnd(); ++j, idx+=2) {
-        const Idx jj(dispatchLDO_index(*j));
-        dcomp matrixElement(rho(ii,jj)=dcomp(flattened(idx),flattened(idx+1)));
-        rho(jj,ii)=conj(matrixElement);
+        dcomp matrixElement(rho(*i)(*j)=dcomp(flattened(idx),flattened(idx+1)));
+        rho(*j)(*i)=conj(matrixElement);
       }
-    }
-  
+
 }
 
 
@@ -124,15 +117,11 @@ densityOperatorize(const LazyDensityOperator<RANK>& matrix)
   typedef cpputils::MultiIndexIterator<RANK> Iterator;
   const Iterator etalon(matrix.getDimensions()-1,cpputils::mii::begin);
   
-  typedef typename DensityOperator<RANK>::Idx Idx;
-  
   for (Iterator i(etalon); i!=etalon.getEnd(); ++i) {
-    const Idx ii(dispatchLDO_index(*i));
-    res(ii,ii)=matrix(ii);
+    res(*i)(*i)=matrix(*i);
     for (Iterator j=++Iterator(i); j!=etalon.getEnd(); ++j) {
-      const Idx jj(dispatchLDO_index(*j));
-      dcomp matrixElement(res(ii,jj)=matrix(ii,jj));
-      res(jj,ii)=conj(matrixElement);
+      dcomp matrixElement(res(*i)(*j)=matrix(*i)(*j));
+      res(*j)(*i)=conj(matrixElement);
     }
   }
 

CPPQEDcore/doc/structureGuide.markdown
@@ -203,9 +203,7 @@ The implementation of the averaging function may read \dontinclude ExampleIntera
 \until averages(3)
 \until }
 \until }
-at this point, the `averages` array contains the real and imaginary parts of \f$\avr{a^\dagger b}\f$ and \f$\avr{a b},\f$ respectively. Note that a quantumdata::LazyDensityOperator of
-arity higher than one can be indexed via the auxiliary quantumdata::LazyDensityOperator::Idx type which represents a multi-index of the corresponding arity (and reduces to a single integer
-in the unary case).
+at this point, the `averages` array contains the real and imaginary parts of \f$\avr{a^\dagger b}\f$ and \f$\avr{a b},\f$ respectively.
 
 Now the desired set of quantum averages can be obtained via linear operations:
 \until }

CPPQEDcore/examples/ExampleInteraction.cc
@@ -18,21 +18,19 @@ hierarchical::InteractionX_X::InteractionX_X(const ModeBase& m0, const ModeBase&
 {}
 
 
-auto hierarchical::InteractionX_X_Correlations::average_v(NoTime, const LazyDensityOperator& matrix) const ->  const Averages
+auto hierarchical::InteractionX_X_Correlations::average_v(NoTime, const LazyDensityOperator& matrix) const -> const Averages
 {
-  typedef LazyDensityOperator::Idx Idx;
-
   Averages averages(4);
   averages=0;
 
   for (int n=0; n<int(matrix.getDimension(0)); n++) for (int m=1; m<int(matrix.getDimension(1)); m++) {
       if(n<int(matrix.getDimension(0))-1) {
-        dcomp temp=sqrt(m*(n+1))*matrix(Idx(n,m),Idx(n+1,m-1));
+        dcomp temp=sqrt(m*(n+1))*matrix(n,m)(n+1,m-1);
         averages(0)+=real(temp);
         averages(1)+=imag(temp);
       }
       if(n>0) {
-        dcomp temp=sqrt(m*n)*matrix(Idx(n,m),Idx(n-1,m-1));
+        dcomp temp=sqrt(m*n)*matrix(n,m)(n-1,m-1);
         averages(2)+=real(temp);
         averages(3)+=imag(temp);
       }

CPPQEDcore/examples/cpcPaperExamples.cc
@@ -38,13 +38,12 @@ calculateASqr(const LazyDensityOperator<1>& m)
 const complex
 calculateADaggerB(const LazyDensityOperator<2>& m)
 {
-  typedef LazyDensityOperator<2>::Idx Idx; 
   // The index type is a blitz::TinyVector<ptrdiff_t,2>
   const LazyDensityOperator<2>::Dimensions dim(m.getDimensions());
 
   complex res;
   for (int i=0; i<dim[0]-1; ++i) for (int j=1; j<dim[1]; ++j)
-    res+=sqrt((i+1)*j)*matrix(Idx(i,j),Idx(i+1,j-1));
+    res+=sqrt((i+1)*j)*matrix(i,j)(i+1,j-1);
   return res;
 }
 

CPPQEDcore/quantumdata/DensityOperator.cc
@@ -1,15 +1,0 @@
-#include "DensityOperator.h"
-
-namespace quantumdata {
-
-template<> 
-inline
-const dcomp 
-DensityOperator<1>::index(const Idx& i, const Idx& j) const
-{
-  return getArray()(i,j);
-}
-
-} // quantumdata
-
-

CPPQEDcore/quantumdata/DensityOperator.h
@@ -87,11 +87,49 @@ public:
 
   template<typename OTHER>
   DensityOperator& operator=(const OTHER& other) {getArray()=other; return *this;}
-  
+
+private:
+  class IndexerProxy
+  {
+  public:
+    IndexerProxy(const DensityOperator* rho, const Idx& firstIndex) : rho_(rho), firstIndex_(firstIndex) {}
+
+    template<typename... SubscriptPack>
+    IndexerProxy(const DensityOperator* rho, int s0, SubscriptPack... subscriptPack) : IndexerProxy(rho,Idx(s0,subscriptPack...))
+    {static_assert( mpl::size<mpl::vector<SubscriptPack...> >::value==RANK-1 , "Incorrect number of subscripts for DensityOperator." );}
+
+    const dcomp& operator()(const Idx& secondIndex) const {return rho_->indexWithTiny(firstIndex_,secondIndex);}
+          dcomp& operator()(const Idx& secondIndex)       {return const_cast<dcomp&>(static_cast<const IndexerProxy&>(*this)(secondIndex));}
+
+    template<typename... SubscriptPack>
+    const dcomp& operator()(int s0, SubscriptPack... subscriptPack) const
+    {
+      static_assert( mpl::size<mpl::vector<SubscriptPack...> >::value==RANK-1 , "Incorrect number of subscripts for DensityOperator::IndexerProxy." );
+      return operator()(Idx(s0,subscriptPack...));
+    }
+
+    template<typename... SubscriptPack>
+          dcomp& operator()(int s0, SubscriptPack... subscriptPack) {return const_cast<dcomp&>(static_cast<const IndexerProxy&>(*this)(s0,subscriptPack...));}
+
+  private:
+    const DensityOperator*const rho_;
+    const Idx firstIndex_;
+
+  };
+
+  friend class IndexerProxy;
+
+public:
   /// \name (Multi-)matrix style indexing
   //@{
-  const dcomp& operator()(const Idx& i, const Idx& j) const;
-        dcomp& operator()(const Idx& i, const Idx& j) {return const_cast<dcomp&>(static_cast<const DensityOperator&>(*this)(i,j));}
+  const IndexerProxy operator()(const Idx& firstIndex) const {return IndexerProxy(this,firstIndex);}
+        IndexerProxy operator()(const Idx& firstIndex)       {return IndexerProxy(this,firstIndex);}
+
+  template<typename... SubscriptPack>
+  const IndexerProxy operator()(int s0, SubscriptPack... subscriptPack) const {return IndexerProxy(this,s0,subscriptPack...);}
+
+  template<typename... SubscriptPack>
+        IndexerProxy operator()(int s0, SubscriptPack... subscriptPack)       {return IndexerProxy(this,s0,subscriptPack...);}
   //@}
 
   /// \name Norm
@@ -122,7 +160,9 @@ public:
   //@}
 
 private:
-  const dcomp index(const Idx& i, const Idx& j) const override {return operator()(i,j);} ///< This function implements the LazyDensityOperator interface in a trivial element-access way
+  const dcomp& indexWithTiny(const Idx&, const Idx&) const; ///< Used for implementing operator() and the index function below.
+
+  const dcomp index(const Idx& i, const Idx& j) const override {return indexWithTiny(i,j);} ///< This function implements the LazyDensityOperator interface in a trivial element-access way
 
   double trace_v() const override {return norm();} ///< A straightforward implementation of a LazyDensityOperator virtual
 

CPPQEDcore/quantumdata/DistributionFunctions.h
@@ -64,7 +64,7 @@ double wignerFunction(const DensityOperatorFunctor& rho, double x, double y, siz
     static dcomp _(const DensityOperatorFunctor& rho, size_t dim, double r, size_t k)
     {
       dcomp res(0.);
-      for (size_t n=0; n<dim-k; ++n) res+=details::w(n,r,k)*rho(n+k,n);
+      for (size_t n=0; n<dim-k; ++n) res+=details::w(n,r,k)*rho(n+k)(n);
       return res;
     }
   };
@@ -90,7 +90,7 @@ double qFunction(const DensityOperator& rho, double x, double y, size_t)
   dcomp qComplex;
   
   for (size_t m=0; m<rho.getDimension(); ++m) for (size_t n=0; n<rho.getDimension(); ++n)
-    qComplex+=coherentElement(n,alpha)*coherentElement(m,conj(alpha))*rho(m,n);
+    qComplex+=coherentElement(n,alpha)*coherentElement(m,conj(alpha))*rho(m)(n);
   
   return exp(-sqrAbs(alpha))*real(qComplex);
 }

CPPQEDcore/quantumdata/LazyDensityOperator.h
@@ -67,15 +67,60 @@ public:
 
   typedef typename Base::Dimensions Dimensions; ///< Inherited from DimensionsBookkeeper
 
-  /// The type used for indexing the “rows” and the “columns”.
-  /** Just an integer (index) if `RANK=1`, otherwise a tiny vector of integers (multi-index). */
-  typedef typename mpl::if_c<(RANK==1),int,IdxTiny<RANK> >::type Idx; 
+  typedef IdxTiny<RANK> Idx; ///< The type used for indexing the “rows” and the “columns”: a tiny vector of integers (multi-index)
 
   virtual ~LazyDensityOperator() {}
 
-  const dcomp operator()(const Idx& i, const Idx& j) const {return index(i,j);} ///< The indexing function calling a purely virtual function according to the non-virtual interface idiom
+private:
+  class IndexerProxy
+  {
+  public:
+    IndexerProxy(const LazyDensityOperator* ldo, const Idx& firstIndex) : ldo_(ldo), firstIndex_(firstIndex) {}
 
-  double operator()(const Idx& i) const {return real((*this)(i,i));} ///< An inline function for conveniently addressing the diagonal elements
+    const dcomp operator()(const Idx& secondIndex) const {return ldo_->index(firstIndex_,secondIndex);}
+
+    template<typename... SubscriptPack>
+    const dcomp operator()(int s0, SubscriptPack... subscriptPack) const
+    {
+      static_assert( mpl::size<mpl::vector<SubscriptPack...> >::value==RANK-1 , "Incorrect number of subscripts for LazyDensityOperator::IndexerProxy." );
+      return operator()(Idx(s0,subscriptPack...));
+    }
+
+    operator double() const {return real(ldo_->index(firstIndex_,firstIndex_));}
+
+  private:
+    const LazyDensityOperator*const ldo_;
+    const Idx firstIndex_;
+
+  };
+
+  friend class IndexerProxy;
+
+public:
+  /// Multi-matrix style indexing via Idx type
+  const IndexerProxy operator()(const Idx& firstIndex) const {return IndexerProxy(this,firstIndex);}
+
+  /// Multi-matrix style indexing via packs of integers
+  /**
+   * This allows for the very convenient indexing syntax (e.g. a ternary LazyDensityOperator `matrix` indexed by multi-indeces `i` and `j`):
+   *
+   *     matrix(i0,i1,i2)(j0,j1,j2)
+   *
+   * while
+   *
+   *     matrix(i0,i1,i2)
+   *
+   * returns a proxy implicitly convertible to a `double`, giving the diagonal element corresponding to the multi-index `i`
+   *
+   * \note The number of indeces in the multi-index is checked @ compile time.
+   *
+   */
+  template<typename... SubscriptPack>
+  const IndexerProxy operator()(int s0, SubscriptPack... subscriptPack) const
+  {
+    static_assert( mpl::size<mpl::vector<SubscriptPack...> >::value==RANK-1 , "Incorrect number of subscripts for LazyDensityOperator." );
+    return operator()(Idx(s0,subscriptPack...));
+  }
 
   double trace() const {return trace_v();} ///< Returns the trace (redirected to a pure virtual)
   
@@ -101,14 +146,6 @@ private:
   virtual double trace_v() const = 0;
 
 };
-
-
-/// Converts the index-tiny of size `RANK` to the arity-dependent indexing type of LazyDensityOperator \related LazyDensityOperator
-template<int RANK>
-inline const typename LazyDensityOperator<RANK>::Idx dispatchLDO_index(const IdxTiny<RANK>& idx) {return idx   ;}
-
-/// ” for `RANK=1` \related LazyDensityOperator
-inline const          LazyDensityOperator<1   >::Idx dispatchLDO_index(const IdxTiny<1   >& idx) {return idx[0];}
 
 
 /// Turns the data of a LazyDensityOperator into a real 1D array
@@ -147,11 +184,10 @@ const DArray<1> deflate(const LazyDensityOperator<RANK>&, bool offDiagonals);
  * 
  *   const dcomp calculateADaggerB(const LazyDensityOperator<2>& matrix)
  *   {
- *     typedef LazyDensityOperator<2>::Idx Idx;
  *     const LazyDensityOperator<2>::Dimensions dim(matrix.getDimensions());
  * 
  *     dcomp res;
- *     for (int i=0; i<dim[0]-1; ++i) for (int j=1; j<dim[1]; ++j) res+=sqrt((i+1)*j)*matrix(Idx(i,j),Idx(i+1,j-1));
+ *     for (int i=0; i<dim[0]-1; ++i) for (int j=1; j<dim[1]; ++j) res+=sqrt((i+1)*j)*matrix(i,j)(i+1,j-1);
  *     return res;
  *   }
  *   ~~~

commit 4c5c90e39209e1c60d2fa83751cfdc8649cf1bc6

Corrected bug introduced in 6a25b988d which prevented workings with increased arity + added a new script to the supported scripts which uses the possibility of increased arity Cf. discussion @ http://sourceforge.net/p/cppqed/mailman/cppqed-support/thread/5358F34D.2050007@physics.ox.ac.uk/

CPPQEDcore/quantumdata/DensityOperator.tcc
@@ -77,7 +77,7 @@ template<int RANK>
 const dcomp&
 DensityOperator<RANK>::indexWithTiny(const Idx& i, const Idx& j) const
 {
-  return getArray()(blitzplusplus::concatenateTinies(i,j));
+  return getArray()(blitzplusplus::concatenateTinies<int,int,RANK,RANK>(i,j));
 }
 
 

CPPQEDscripts/6qbits.cc
@@ -1,0 +1,57 @@
+//#define BOOST_MPL_CFG_NO_PREPROCESSED_HEADERS // cf. http://www.boost.org/doc/libs/1_51_0/libs/mpl/doc/refmanual/limit-vector-size.html
+//#define BOOST_MPL_LIMIT_VECTOR_SIZE 30 // must be rounded to the nearest multiple of 10
+//#define FUSION_MAX_VECTOR_SIZE 21 // for 10qbits
+#define BLITZ_ARRAY_LARGEST_RANK 14
+#define QUANTUMOPERATOR_TRIDIAGONAL_MAX_RANK 5 // 14 as maximal rank would be an overkill here
+
+#include "EvolutionComposite.h"
+
+#include "AveragingUtils.tcc"
+#include "JaynesCummings.h"
+#include "QbitModeCorrelations.h"
+
+using namespace std;
+
+typedef quantumdata::StateVector<7> StateVector;
+
+
+int main(int argc, char* argv[])
+{
+  // ****** Parameters of the Problem
+
+  ParameterTable p;
+
+  evolution::Pars pe(p); // Driver Parameters
+  qbit::ParsPumpedLossy pplqb(p);
+  mode::ParsPumpedLossy pplm (p); 
+  jaynescummings::Pars  pjc  (p);
+
+  // Parameter finalization
+  auto qmp=updateWithPicture(p,argc,argv,"--");
+  
+  // ****** ****** ****** ****** ****** ******
+
+  const qbit::Ptr qbit(qbit::make(pplqb,qmp));
+
+  const mode::Ptr mode(mode::make<mode::AveragedQuadratures>(pplm ,qmp));
+
+  const jaynescummings::Ptr jc(jaynescummings::make(qbit,mode,pjc)),
+                            jcRDO(jaynescummings::make<ReducedDensityOperatorNegativity<2,tmptools::Vector<0> > >(qbit,mode,pjc,"JaynesCummings1-6",jc->getDimensions())),
+                            jcCorr(jaynescummings::make<QbitModeCorrelations>(qbit,mode,pjc));
+                            
+  
+  StateVector psi(qbit::init(pplqb)*qbit::init(pplqb)*qbit::init(pplqb)*qbit::init(pplqb)*qbit::init(pplqb)*qbit::init(pplqb)*mode::init(pplm));
+  psi.renorm();
+
+  evolve(psi,composite::make(Act<0,6>(jcCorr),
+                             Act<1,6>(jcRDO),
+                             Act<2,6>(jc),
+                             Act<3,6>(jc),
+                             Act<4,6>(jc),
+                             Act<5,6>(jc)),
+         pe);
+ 
+
+
+
+}