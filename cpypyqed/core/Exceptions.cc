4f5e17533108d01fae93274dc3bc4b8987fc7fbd
Raimar Sandner <raimar.sandner@uibk.ac.at>
2015-10-30 18:31:17 +0100

    register InfinateDetectedException with python

---
+++ cpypyqed/core/Exceptions.cc
@@ -0,0 +1,27 @@
+// Copyright Raimar Sandner 2012–2015. Distributed under the Boost Software License, Version 1.0. (See accompanying file LICENSE.txt)
+// -*- C++ -*-
+
+#include "Core.h"
+
+#include "LiouvilleanAveragedCommon.h"
+
+#include <boost/python/exception_translator.hpp>
+
+
+using namespace boost::python;
+
+namespace pythonext {
+
+namespace {
+void pyInfiniteDetectedException(const structure::InfiniteDetectedException &)
+{
+  PyErr_SetString(PyExc_RuntimeError, "InfinateDetectedException in C++QED");
+}
+}
+
+void export_io()
+{
+  register_exception_translator<structure::InfiniteDetectedException>(&pyInfiniteDetectedException);
+}
+
+} // pythonext
