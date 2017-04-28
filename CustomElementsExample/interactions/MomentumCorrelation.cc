1889c34177d5b69708b63512325c9727d5002284
András Vukics <vukics.andras@wigner.mta.hu>
2017-04-28 09:55:09 +0200

    Removed unused variables & parameter

--- CustomElementsExample/interactions/MomentumCorrelation.cc
+++ CustomElementsExample/interactions/MomentumCorrelation.cc
@@ -9,12 +9,13 @@ MomentumCorrelation::MomentumCorrelation(particle::Ptr p0, particle::Ptr p1) :
   EA_Base("MomentumCorrelation",{"<P1P2>"})
 {}
 
-const MomentumCorrelation::Averages MomentumCorrelation::average_v(Time t, const LazyDensityOperator& matrix) const
+const MomentumCorrelation::Averages MomentumCorrelation::average_v(Time, const LazyDensityOperator& matrix) const
 {
   auto averages(initializedAverages());
-  double diag,temp;
+
   size_t dim1 = matrix.getDimensions()[0];
   size_t dim2 = matrix.getDimensions()[1];
+
   for (int n=0; n < dim1; n++)
     for (int m=0; m < dim2;m++) {
       averages(0)+=(-(dim1/2.)+n)*(-(dim2/2.)+m)*matrix(n,m);
