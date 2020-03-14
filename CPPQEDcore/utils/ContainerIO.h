a4ced3b593ff6cb5fa2b417aa4a4e2083aa986f4
Vukics <vukics@Hierosolyma.localdomain>
2020-03-14 23:06:29 +0100

    hana::tuple output now works

--- CPPQEDcore/utils/ContainerIO.h
+++ CPPQEDcore/utils/ContainerIO.h
@@ -13,8 +13,11 @@
 // Code retrieved from https://github.com/louisdx/cxx-prettyprint on 13/03/2020
 
 
-#ifndef H_PRETTY_PRINT
-#define H_PRETTY_PRINT
+#ifndef CONTAINER_IO_H_INCLUDED
+#define CONTAINER_IO_H_INCLUDED
+
+#include <boost/hana/tuple.hpp>
+#include <boost/hana/for_each.hpp>
 
 #include <cstddef>
 #include <iterator>
@@ -27,49 +30,45 @@
 #include <utility>
 #include <valarray>
 
-namespace pretty_print {
+namespace container_io {
 
 namespace detail {
 // SFINAE type trait to detect whether T::const_iterator exists.
 
 struct sfinae_base
 {
-    using yes = char;
-    using no  = yes[2];
+  using yes = char;
+  using no  = yes[2];
 };
 
 template <typename T>
 struct has_const_iterator : private sfinae_base
 {
 private:
-    template <typename C> static yes & test(typename C::const_iterator*);
-    template <typename C> static no  & test(...);
+  template <typename C> static yes & test(typename C::const_iterator*);
+  template <typename C> static no  & test(...);
 public:
-    static const bool value = sizeof(test<T>(nullptr)) == sizeof(yes);
-    using type =  T;
+  static const bool value = sizeof(test<T>(nullptr)) == sizeof(yes);
+  using type =  T;
 };
 
 template <typename T>
 struct has_begin_end : private sfinae_base
 {
 private:
-    template <typename C>
-    static yes & f(typename std::enable_if<
-        std::is_same<decltype(static_cast<typename C::const_iterator(C::*)() const>(&C::begin)),
-                      typename C::const_iterator(C::*)() const>::value>::type *);
+  template <typename C>
+  static yes & f(typename std::enable_if<std::is_same<decltype(static_cast<typename C::const_iterator(C::*)() const>(&C::begin)),typename C::const_iterator(C::*)() const>::value>::type *);
 
-    template <typename C> static no & f(...);
+  template <typename C> static no & f(...);
 
-    template <typename C>
-    static yes & g(typename std::enable_if<
-        std::is_same<decltype(static_cast<typename C::const_iterator(C::*)() const>(&C::end)),
-                      typename C::const_iterator(C::*)() const>::value, void>::type*);
+  template <typename C>
+  static yes & g(typename std::enable_if<std::is_same<decltype(static_cast<typename C::const_iterator(C::*)() const>(&C::end)),typename C::const_iterator(C::*)() const>::value, void>::type*);
 
-    template <typename C> static no & g(...);
+  template <typename C> static no & g(...);
 
 public:
-    static bool const beg_value = sizeof(f<T>(nullptr)) == sizeof(yes);
-    static bool const end_value = sizeof(g<T>(nullptr)) == sizeof(yes);
+  static bool const beg_value = sizeof(f<T>(nullptr)) == sizeof(yes);
+  static bool const end_value = sizeof(g<T>(nullptr)) == sizeof(yes);
 };
 
 }  // namespace detail
@@ -80,10 +79,10 @@ public:
 template <typename TChar>
 struct delimiters_values
 {
-    using char_type = TChar;
-    const char_type * prefix;
-    const char_type * delimiter;
-    const char_type * postfix;
+  using char_type = TChar;
+  const char_type * prefix;
+  const char_type * delimiter;
+  const char_type * postfix;
 };
 
 
@@ -92,8 +91,8 @@ struct delimiters_values
 template <typename T, typename TChar>
 struct delimiters
 {
-    using type = delimiters_values<TChar>;
-    static const type values; 
+  using type = delimiters_values<TChar>;
+  static const type values; 
 };
 
 
@@ -107,52 +106,45 @@ template <typename T,
           typename TDelimiters = delimiters<T, TChar>>
 struct print_container_helper
 {
-    using delimiters_type = TDelimiters;
-    using ostream_type = std::basic_ostream<TChar, TCharTraits>;
+  using delimiters_type = TDelimiters;
+  using ostream_type = std::basic_ostream<TChar, TCharTraits>;
 
-    template <typename U>
-    struct printer
+  template <typename U>
+  struct printer
+  {
+    static void print_body(const U & c, ostream_type & stream)
     {
-        static void print_body(const U & c, ostream_type & stream)
-        {
-            using std::begin;
-            using std::end;
+      using std::begin;
+      using std::end;
 
-            auto it = begin(c);
-            const auto the_end = end(c);
+      auto it = begin(c);
+      const auto the_end = end(c);
 
-            if (it != the_end)
-            {
-                for ( ; ; )
-                {
-                    stream << *it;
-
-                if (++it == the_end) break;
-
-                if (delimiters_type::values.delimiter != NULL)
-                    stream << delimiters_type::values.delimiter;
-                }
-            }
+      if (it != the_end) {
+        for ( ; ; ) {
+          stream << *it;
+          
+          if (++it == the_end) break;
+          
+          if (delimiters_type::values.delimiter != NULL) stream << delimiters_type::values.delimiter;
         }
-    };
-
-    print_container_helper(const T & container)
-    : container_(container)
-    { }
-
-    inline void operator()(ostream_type & stream) const
-    {
-        if (delimiters_type::values.prefix != NULL)
-            stream << delimiters_type::values.prefix;
-
-        printer<T>::print_body(container_, stream);
-
-        if (delimiters_type::values.postfix != NULL)
-            stream << delimiters_type::values.postfix;
+      }
     }
+  };
+
+  print_container_helper(const T & container) : container_(container) {}
+
+  inline void operator()(ostream_type & stream) const
+  {
+    if (delimiters_type::values.prefix != NULL) stream << delimiters_type::values.prefix;
+
+    printer<T>::print_body(container_, stream);
+
+    if (delimiters_type::values.postfix != NULL) stream << delimiters_type::values.postfix;
+  }
 
 private:
-    const T & container_;
+  const T & container_;
 };
 
 // Specialization for pairs
@@ -161,15 +153,15 @@ template <typename T, typename TChar, typename TCharTraits, typename TDelimiters
 template <typename T1, typename T2>
 struct print_container_helper<T, TChar, TCharTraits, TDelimiters>::printer<std::pair<T1, T2>>
 {
-    using ostream_type = typename print_container_helper<T, TChar, TCharTraits, TDelimiters>::ostream_type;
+  using ostream_type = typename print_container_helper<T, TChar, TCharTraits, TDelimiters>::ostream_type;
 
-    static void print_body(const std::pair<T1, T2> & c, ostream_type & stream)
-    {
-        stream << c.first;
-        if (print_container_helper<T, TChar, TCharTraits, TDelimiters>::delimiters_type::values.delimiter != NULL)
-            stream << print_container_helper<T, TChar, TCharTraits, TDelimiters>::delimiters_type::values.delimiter;
-        stream << c.second;
-    }
+  static void print_body(const std::pair<T1, T2> & c, ostream_type & stream)
+  {
+    stream << c.first;
+    if (print_container_helper<T, TChar, TCharTraits, TDelimiters>::delimiters_type::values.delimiter != NULL)
+        stream << print_container_helper<T, TChar, TCharTraits, TDelimiters>::delimiters_type::values.delimiter;
+    stream << c.second;
+  }
 };
 
 // Specialization for tuples
@@ -178,48 +170,68 @@ template <typename T, typename TChar, typename TCharTraits, typename TDelimiters
 template <typename ...Args>
 struct print_container_helper<T, TChar, TCharTraits, TDelimiters>::printer<std::tuple<Args...>>
 {
-    using ostream_type = typename print_container_helper<T, TChar, TCharTraits, TDelimiters>::ostream_type;
-    using element_type = std::tuple<Args...>;
+  using ostream_type = typename print_container_helper<T, TChar, TCharTraits, TDelimiters>::ostream_type;
+  using element_type = std::tuple<Args...>;
 
-    template <std::size_t I> struct Int { };
+  template <std::size_t I> struct Int { };
 
-    static void print_body(const element_type & c, ostream_type & stream)
-    {
-        tuple_print(c, stream, Int<0>());
-    }
+  static void print_body(const element_type & c, ostream_type & stream)
+  {
+    tuple_print(c, stream, Int<0>());
+  }
 
-    static void tuple_print(const element_type &, ostream_type &, Int<sizeof...(Args)>)
-    {
-    }
+  static void tuple_print(const element_type &, ostream_type &, Int<sizeof...(Args)>) {}
 
-    static void tuple_print(const element_type & c, ostream_type & stream,
-                            typename std::conditional<sizeof...(Args) != 0, Int<0>, std::nullptr_t>::type)
-    {
-        stream << std::get<0>(c);
-        tuple_print(c, stream, Int<1>());
-    }
+  static void tuple_print(const element_type & c, ostream_type & stream,
+                          typename std::conditional<sizeof...(Args) != 0, Int<0>, std::nullptr_t>::type)
+  {
+    stream << std::get<0>(c);
+    tuple_print(c, stream, Int<1>());
+  }
 
-    template <std::size_t N>
-    static void tuple_print(const element_type & c, ostream_type & stream, Int<N>)
-    {
-        if (print_container_helper<T, TChar, TCharTraits, TDelimiters>::delimiters_type::values.delimiter != NULL)
-            stream << print_container_helper<T, TChar, TCharTraits, TDelimiters>::delimiters_type::values.delimiter;
+  template <std::size_t N>
+  static void tuple_print(const element_type & c, ostream_type & stream, Int<N>)
+  {
+    if (print_container_helper<T, TChar, TCharTraits, TDelimiters>::delimiters_type::values.delimiter != NULL)
+      stream << print_container_helper<T, TChar, TCharTraits, TDelimiters>::delimiters_type::values.delimiter;
 
-        stream << std::get<N>(c);
+    stream << std::get<N>(c);
 
-        tuple_print(c, stream, Int<N + 1>());
-    }
+    tuple_print(c, stream, Int<N + 1>());
+  }
 };
+
+
+// Specialization for hana::tuple
+
+template <typename T, typename TChar, typename TCharTraits, typename TDelimiters>
+template <typename ...Args>
+struct print_container_helper<T, TChar, TCharTraits, TDelimiters>::printer<boost::hana::tuple<Args...>>
+{
+  using ostream_type = typename print_container_helper<T, TChar, TCharTraits, TDelimiters>::ostream_type;
+  using element_type = boost::hana::tuple<Args...>;
+
+  static void print_body(const element_type & c, ostream_type & stream)
+  {
+    unsigned i=0;
+    
+    boost::hana::for_each(c, [&](const auto& member) {
+      if (i && print_container_helper<T, TChar, TCharTraits, TDelimiters>::delimiters_type::values.delimiter != NULL) stream << print_container_helper<T, TChar, TCharTraits, TDelimiters>::delimiters_type::values.delimiter;
+      ++i;
+      stream << member ;
+    });
+  }
+
+};
+
 
 // Prints a print_container_helper to the specified stream.
 
 template<typename T, typename TChar, typename TCharTraits, typename TDelimiters>
-inline std::basic_ostream<TChar, TCharTraits> & operator<<(
-    std::basic_ostream<TChar, TCharTraits> & stream,
-    const print_container_helper<T, TChar, TCharTraits, TDelimiters> & helper)
+inline std::basic_ostream<TChar, TCharTraits> & operator<<(std::basic_ostream<TChar, TCharTraits> & stream, const print_container_helper<T, TChar, TCharTraits, TDelimiters> & helper)
 {
-    helper(stream);
-    return stream;
+  helper(stream);
+  return stream;
 }
 
 
@@ -245,6 +257,9 @@ struct is_container<std::pair<T1, T2>> : std::true_type { };
 
 template <typename ...Args>
 struct is_container<std::tuple<Args...>> : std::true_type { };
+
+template <typename ...Args>
+struct is_container<boost::hana::tuple<Args...>> : std::true_type { };
 
 
 // Default delimiters
@@ -321,47 +336,47 @@ template <typename ...Args> const delimiters_values<wchar_t> delimiters< ::std::
 
 // Type-erasing helper class for easy use of custom delimiters.
 // Requires TCharTraits = std::char_traits<TChar> and TChar = char or wchar_t, and MyDelims needs to be defined for TChar.
-// Usage: "cout << pretty_print::custom_delims<MyDelims>(x)".
+// Usage: "cout << container_io::custom_delims<MyDelims>(x)".
 
 struct custom_delims_base
 {
-    virtual ~custom_delims_base() { }
-    virtual std::ostream & stream(::std::ostream &) = 0;
-    virtual std::wostream & stream(::std::wostream &) = 0;
+  virtual ~custom_delims_base() { }
+  virtual std::ostream & stream(::std::ostream &) = 0;
+  virtual std::wostream & stream(::std::wostream &) = 0;
 };
 
 template <typename T, typename Delims>
 struct custom_delims_wrapper : custom_delims_base
 {
-    custom_delims_wrapper(const T & t_) : t(t_) { }
+  custom_delims_wrapper(const T & t_) : t(t_) { }
 
-    std::ostream & stream(std::ostream & s)
-    {
-        return s << print_container_helper<T, char, std::char_traits<char>, Delims>(t);
-    }
+  std::ostream & stream(std::ostream & s)
+  {
+    return s << print_container_helper<T, char, std::char_traits<char>, Delims>(t);
+  }
 
-    std::wostream & stream(std::wostream & s)
-    {
-        return s << print_container_helper<T, wchar_t, std::char_traits<wchar_t>, Delims>(t);
-    }
+  std::wostream & stream(std::wostream & s)
+  {
+    return s << print_container_helper<T, wchar_t, std::char_traits<wchar_t>, Delims>(t);
+  }
 
 private:
-    const T & t;
+  const T & t;
 };
 
 template <typename Delims>
 struct custom_delims
 {
-    template <typename Container>
-    custom_delims(const Container & c) : base(new custom_delims_wrapper<Container, Delims>(c)) { }
+  template <typename Container>
+  custom_delims(const Container & c) : base(new custom_delims_wrapper<Container, Delims>(c)) { }
 
-    std::unique_ptr<custom_delims_base> base;
+  std::unique_ptr<custom_delims_base> base;
 };
 
 template <typename TChar, typename TCharTraits, typename Delims>
 inline std::basic_ostream<TChar, TCharTraits> & operator<<(std::basic_ostream<TChar, TCharTraits> & s, const custom_delims<Delims> & p)
 {
-    return p.base->stream(s);
+  return p.base->stream(s);
 }
 
 
@@ -371,16 +386,16 @@ inline std::basic_ostream<TChar, TCharTraits> & operator<<(std::basic_ostream<TC
 template<typename T>
 struct array_wrapper_n
 {
-    typedef const T * const_iterator;
-    typedef T value_type;
+  typedef const T * const_iterator;
+  typedef T value_type;
 
-    array_wrapper_n(const T * const a, size_t n) : _array(a), _n(n) { }
-    inline const_iterator begin() const { return _array; }
-    inline const_iterator end() const { return _array + _n; }
+  array_wrapper_n(const T * const a, size_t n) : _array(a), _n(n) { }
+  inline const_iterator begin() const { return _array; }
+  inline const_iterator end() const { return _array + _n; }
 
 private:
-    const T * const _array;
-    size_t _n;
+  const T * const _array;
+  size_t _n;
 };
 
 
@@ -390,41 +405,41 @@ private:
 template <typename T>
 struct bucket_print_wrapper
 {
-    typedef typename T::const_local_iterator const_iterator;
-    typedef typename T::size_type size_type;
+  typedef typename T::const_local_iterator const_iterator;
+  typedef typename T::size_type size_type;
 
-    const_iterator begin() const
-    {
-        return m_map.cbegin(n);
-    }
+  const_iterator begin() const
+  {
+    return m_map.cbegin(n);
+  }
 
-    const_iterator end() const
-    {
-        return m_map.cend(n);
-    }
+  const_iterator end() const
+  {
+    return m_map.cend(n);
+  }
 
-    bucket_print_wrapper(const T & m, size_type bucket) : m_map(m), n(bucket) { }
+  bucket_print_wrapper(const T & m, size_type bucket) : m_map(m), n(bucket) { }
 
 private:
-    const T & m_map;
-    const size_type n;
+  const T & m_map;
+  const size_type n;
 };
 
-}   // namespace pretty_print
+} // container_io
 
 
 // Global accessor functions for the convenience wrappers
 
 template<typename T>
-inline pretty_print::array_wrapper_n<T> pretty_print_array(const T * const a, size_t n)
+inline container_io::array_wrapper_n<T> pretty_print_array(const T * const a, size_t n)
 {
-    return pretty_print::array_wrapper_n<T>(a, n);
+  return container_io::array_wrapper_n<T>(a, n);
 }
 
-template <typename T> pretty_print::bucket_print_wrapper<T>
+template <typename T> container_io::bucket_print_wrapper<T>
 bucket_print(const T & m, typename T::size_type n)
 {
-    return pretty_print::bucket_print_wrapper<T>(m, n);
+  return container_io::bucket_print_wrapper<T>(m, n);
 }
 
 
@@ -436,15 +451,29 @@ namespace std
 // Prints a container to the stream using default delimiters
 
 template<typename T, typename TChar, typename TCharTraits>
-inline typename enable_if< ::pretty_print::is_container<T>::value,
-                          basic_ostream<TChar, TCharTraits> &>::type
+inline typename enable_if< ::container_io::is_container<T>::value, basic_ostream<TChar, TCharTraits> &>::type
 operator<<(basic_ostream<TChar, TCharTraits> & stream, const T & container)
 {
-    return stream << ::pretty_print::print_container_helper<T, TChar, TCharTraits>(container);
+  return stream << ::container_io::print_container_helper<T, TChar, TCharTraits>(container);
 }
 
 } // std
 
 
+namespace boost { namespace hana
+{
 
-#endif  // H_PRETTY_PRINT
+// Prints a container to the stream using default delimiters
+
+template<typename TChar, typename TCharTraits, typename ...Args>
+inline auto &
+operator<<(::std::basic_ostream<TChar, TCharTraits> & stream, const boost::hana::tuple<Args...> & container)
+{
+  return stream << ::container_io::print_container_helper<boost::hana::tuple<Args...>, TChar, TCharTraits>(container);
+}
+
+} } // boost::hana
+
+
+
+#endif  // CONTAINER_IO_H_INCLUDED
