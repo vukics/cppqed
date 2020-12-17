d23896379011affddfc2a9a720888a1814736dab
András Vukics <andras.vukics@gmail.com>
2020-12-17 14:11:06 +0100

    Clearing the field.

--- cpypyqed/core/Core.h
+++
@@ -1,9 +0,0 @@
-// Copyright Raimar Sandner 2012–2020. Distributed under the Boost Software License, Version 1.0. (See accompanying file LICENSE.txt)
-
-#ifndef CPYPYQED_CORE_CORE_H_INCLUDED
-#define CPYPYQED_CORE_CORE_H_INCLUDED
-
-#include "PythonExtension.h"
-#include "Namespaces.h"
-
-#endif // CPYPYQED_CORE_CORE_H_INCLUDED
