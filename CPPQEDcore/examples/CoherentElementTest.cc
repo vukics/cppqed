6623e761e428b7109088e19aa27be5dd7246a0e8
András Vukics <andras.vukics@gmail.com>
2021-04-07 16:16:03 +0200

    All tests compile & run – MCWF_Trajectory produces const 0 dtDid in some circumstances

--- CPPQEDcore/examples/CoherentElementTest.cc
+++ CPPQEDcore/examples/CoherentElementTest.cc
@@ -6,7 +6,7 @@
 
 using namespace std;
 using namespace boost::math;
-using namespace mathutils;
+using namespace cppqedutils;
 
 int main(int argc, char* argv[])
 {
--- Testing/boost/LazyDensityOperatorFFT.cc
+++ Testing/boost/LazyDensityOperatorFFT.cc
@@ -20,20 +20,20 @@ BOOST_AUTO_TEST_CASE( LAZY_DENSITY_OPERATOR_FFTRANSFORM )
   using namespace particle;
   using namespace quantumdata;
   using namespace blitz;
-  using mathutils::fcmp;
+  using cppqedutils::fcmp;
   InitialCondition ic1(0.5,0,1,true);
   InitialCondition ic2(0.5,0,2,true);
   InitialCondition ic3(0.5,0,3,true);
   Spatial spacial(3);
   SV3 psi1 = wavePacket(ic1,spacial,true)*wavePacket(ic2,spacial,true)*wavePacket(ic3,spacial,true);
   SV3 psi2 = wavePacket(ic1,spacial,false)*wavePacket(ic2,spacial,true)*wavePacket(ic3,spacial,false);
-  LDO3::Ptr psiX = ffTransform<Vector<0,2> >(psi1,fft::DIR_KX);
+  auto psiX = ffTransform<Vector<0,2> >(psi1,fft::DIR_KX);
   const SV3 *psi3 = dynamic_cast<const SV3 *>(psiX.get());
   BOOST_CHECK(max(abs(psi2.getArray()-psi3->getArray()))<eps);
   
   DO3 rho1 = dyad(psi1,psi1);
   DO3 rho2 = dyad(psi2,psi2);
-  LDO3::Ptr rhoX = ffTransform<Vector<0,2> >(rho1,fft::DIR_KX);
+  auto rhoX = ffTransform<Vector<0,2> >(rho1,fft::DIR_KX);
   const DO3 *rho3 = dynamic_cast<const DO3 *>(rhoX.get());
   BOOST_CHECK(max(abs(rho2.getArray()-rho3->getArray()))<eps);
 }
