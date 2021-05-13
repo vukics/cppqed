04c5690c8153318f89a2a33030ea7f357d92f152
András Vukics <andras.vukics@gmail.com>
2021-05-13 21:45:50 +0200

    SliceIterator can be instantiated for StateVector and DensityOperator in now top-level compilation tests.

---
+++ Testing/compile/SliceIteratorQuantumData.cc
@@ -0,0 +1,8 @@
+#include "DensityOperator.h"
+#include "StateVector.h"
+
+
+template class cppqedutils::SliceIterator<quantumdata::StateVector,6,tmptools::Vector<3,2,5,1>>;
+
+template class cppqedutils::SliceIterator<quantumdata::DensityOperator,6,tmptools::Vector<3,2,5,1>>;
+
