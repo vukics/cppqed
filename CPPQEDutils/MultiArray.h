310177400f2ef401fc5e82971fbed3583dbd7cbe
András Vukics <andras.vukics@gmail.com>
2022-04-08 12:56:07 +0200

    Good progress with MultiArray

--- CPPQEDutils/MultiArray.h
+++ CPPQEDutils/MultiArray.h
@@ -5,47 +5,164 @@
 
 #include <array>
 #include <concepts>
+#include <functional>
+#include <numeric>
 #include <span>
+#include <stdexcept>
+#include <tuple>
 #include <vector>
 
 
 namespace cppqedutils {
+
+  
+template <unsigned RANK>
+using Extents=std::array<size_t,RANK>;
+
+
+namespace multiarray {
+
+/// Checking the consistency of template arguments for use in slicing
+/**
+ * - size of `RetainedAxes` must not be larger than `RANK`
+ * - `RetainedAxes` must not contain duplicated elements
+ * - all elements of `RetainedAxes` must be smaller than `RANK`
+ * 
+ * \see \ref specifyingsubsystems
+ * 
+ */
+template <int RANK, typename RetainedAxes>
+constexpr bool consistent(RetainedAxes ra) // if the optional argument is not given, RetainedAxes needs to be default-constructible 
+{
+  using namespace hana; using namespace literals;
+
+  constexpr bool res = maximum(ra) < int_c<RANK> && minimum(ra) >= 0_c;
+  
+  if constexpr ( Sequence<RetainedAxes>::value ) {
+    constexpr auto sorted = sort(ra);
+    res &= (unique(sorted) == sorted);    
+  }
+  
+  return res;
+}
+
+} // multiarray
 
 
 template <typename T, unsigned RANK>
 class MultiArrayView
 {
 public:
-  /*
-  template <typename... Indices>
-  requires ( sizeof...(Indices)==RANK && ( ... && std::is_convertible_v<Indices,size_t> ) )
-  T& operator() (Indices... i);
-  */
+  MultiArrayView(const MultiArrayView&) = default; MultiArrayView(MultiArrayView&&) = default;
   
-  T& operator() (std::convertible_to<size_t> auto ... i) requires (sizeof...(i)==RANK)
+  MultiArrayView& operator=(const MultiArrayView&) = default; MultiArrayView& operator=(MultiArrayView&&) = default;
+  
+  /* template <typename... Indices> requires ( sizeof...(Indices)==RANK && ( ... && std::is_convertible_v<Indices,size_t> ) ) T& operator() (Indices... i); */
+  
+  MultiArrayView(const Extents<RANK>& extents, const Extents<RANK>& strides, size_t offset, auto&&... spanArgs)
+  : extents_{extents}, strides_{strides}, offset_{offset}, dataView_{std::forward<decltype(spanArgs)>(spanArgs)...} {}
+  
+  T& operator() (const Extents<RANK>& indices) const
   {
 #ifndef   NDEBUG
-    hana::for_each(tmptools::ordinals<RANK>,[](const auto&) {});
+    hana::for_each(tmptools::ordinals<RANK>,[&](auto idx) {
+      if ( size_t actualIndex=indices[idx], actualExtent=extents_[idx]; actualIndex >= actualExtent )
+        throw std::range_error("Index position: "+std::to_string(idx)+", index value: "+std::to_string(actualIndex)+", extent: "+std::to_string(actualExtent));
+    });
 #endif // NDEBUG
+    return dataView_[hana::fold(tmptools::ordinals<RANK>,offset_,[&](size_t v,auto idx) {return v+strides_[idx]*indices[idx];})];
+    
   }
   
+  T& operator() (std::convertible_to<size_t> auto ... i) const
+  requires (sizeof...(i)==RANK)
+  {
+    return operator()(Extents<RANK>{i...});
+  }
+  
+  /// A simple specialization for unary views
+  T& operator() (std::convertible_to<size_t> auto i) const requires (RANK==1) {return dataView_[offset_+strides_[0]*i];}
+
+  /// A simple specialization for binary views
+  T& operator() (std::convertible_to<size_t> auto i, std::convertible_to<size_t> auto j) const requires (RANK==2) {return dataView_[offset_+strides_[0]*i+strides_[1]*j];}
+
+  
+  template <typename RetainedAxes>
+  auto slicesRange(RetainedAxes ra, std::vector<size_t>&& offsets) const requires (multiarray::consistent<RANK>(ra))
+  {
+    std::vector<MultiArrayView<T,hana::size(ra)>> res;
+  }
+  
+ 
 private:
+  Extents<RANK> extents_, strides_;
+
   size_t offset_;
   
-  std::array<size_t,RANK> extents_, strides_;
-  
-  std::span<T> dataView_;
+protected:
+  std::span<T> dataView_; // the storage can eventually become a kokkos::View
   
 };
+
+
+namespace multiarray {
+
+template <unsigned RANK>
+auto calculateStrides(const Extents<RANK>& extents)
+{
+  Extents<RANK> strides; strides[0]=1;
+  for (size_t i=1; i<RANK; ++i) strides[i]=strides[i-1]*extents[i-1];
+  return strides;
+}
+
+} // multiarray
+
 
 
 template <typename T, unsigned RANK>
 class MultiArray : public MultiArrayView<T,RANK>
 {
+public:
+  using StorageType = std::vector<T>;
+  
+  MultiArray(const MultiArray&) = delete; MultiArray& operator=(const MultiArray&) = delete;
+  
+  MultiArray(MultiArray&&) = default; MultiArray& operator=(MultiArray&&) = default;
+  
+  template<typename INITIALIZER=std::function<void(StorageType&)>>
+  MultiArray(const Extents<RANK>& extents, INITIALIZER&& initializer=[](StorageType&) {})
+  : MultiArrayView<T,RANK>{extents,multiarray::calculateStrides<RANK>(extents),0},
+  data_(std::accumulate(extents.begin(),extents.end(),1ul,std::multiplies{}))
+  {
+    this->dataView_=std::span<T>(data_);
+    initializer(data_); // initialize vector
+  }
+
+  
 private:
-  std::vector<T> data_;
+  StorageType data_;
   
 };
+
+
+namespace multiarray {
+
+template <unsigned RANK, typename RetainedAxes>
+auto calculateSlicesOffsets(Extents<RANK> extents, Extents<RANK> strides, RetainedAxes ra)
+{
+  std::vector<size_t> res;
+  return res;
+}
+
+
+template <unsigned RANK, typename RetainedAxes>
+auto calculateSlicesOffsets(Extents<RANK> extents, RetainedAxes ra)
+{
+  return calculateSlicesOffsets(extents,calculateStrides(extents),ra);
+}
+
+
+} // multiarray
 
 
 } // cppqedutils
--- CPPQEDutils/sandbox/MultiArray.cc
+++ CPPQEDutils/sandbox/MultiArray.cc
@@ -5,16 +5,26 @@
 
 int main()
 {
-  cppqedutils::MultiArray<double,5> ma;
+  std::vector<double> vec(1000);
+  
+  std::span<double> span(vec);
+  
+  std::cerr<<vec.size()<<" "<<span.size()<<std::endl;
+  
+  cppqedutils::MultiArray<double,5> ma{{10,11,13,9,8}};
   
   double& v1=ma(1,5,6,3,4) ;
+  
+  std::cout<<v1<<std::endl;
+  
+  std::cout<<ma(10,5,6,3,4)<<std::endl;
   // double& v2=ma(1,5,6,std::tuple<int>{3},4) ;
 
   // cppqedutils::MultiArray<const double,5> mac; // "std::vector must have a non-const, non-volatile value_type"
 
-  cppqedutils::MultiArrayView<const double,5> mavc;
+  // cppqedutils::MultiArrayView<const double,5> mavc;
 
-  const double& v3=mavc(1,5,6,3,4) ;
+  // const double& v3=mavc(1,5,6,3,4) ;
   // double& v4=mavc(1,5,6,3,4) ;
   
   // double& v5=ma(1,5,6,3) ;
