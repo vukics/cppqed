bd657ab043fb995aaf4c2dc4b95eeb570a83dc0a
András Vukics <andras.vukics@gmail.com>
2022-05-31 14:27:49 +0200

    Added tentative MultiDiagonal

---
+++ CPPQEDcore/quantumoperator/MultiDiagonal.h
@@ -0,0 +1,20 @@
+// Copyright András Vukics 2022. Distributed under the Boost Software License, Version 1.0. (See accompanying file LICENSE.txt)
+#pragma once
+
+#include "MultiArray.h"
+
+
+namespace quantumoperator {
+  
+
+/// `ListOfOffsets` should be a `hana::tuple` of `hana::tuple`s of `size_t`s
+template <typename ListOfOffsets>
+class MultiDiagonal
+{
+public:
+  static constexpr size_t N_RANK=hana::size(ListOfOffsets{});
+  
+};
+  
+  
+} // quantumoperator
