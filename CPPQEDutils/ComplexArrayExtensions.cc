71e4bdd85e79587769ad405083aba90b8b0680ee
András Vukics <andras.vukics@gmail.com>
2022-09-16 11:05:06 +0200

    Further cleanup

--- CPPQEDutils/ComplexArrayExtensions.cc
+++
@@ -1,8 +0,0 @@
-// Copyright András Vukics 2006–2022. Distributed under the Boost Software License, Version 1.0. (See accompanying file LICENSE.txt)
-#include "ComplexArrayExtensions.h"
-
-using namespace linalg;
-using namespace blitz::tensor;
-
-template<> void blitzplusplus::dodirect::_<true >(CMatrix& m, const CVector& v1, const CVector& v2) {m=v1(i)*v2(j);}
-template<> void blitzplusplus::dodirect::_<false>(CMatrix& m, const CVector& v1, const CVector& v2) {m=v1(i)+v2(j);}
--- CPPQEDutils/LinearAlgebra.h
+++ CPPQEDutils/LinearAlgebra.h
@@ -1,16 +1,20 @@
 // Copyright András Vukics 2006–2022. Distributed under the Boost Software License, Version 1.0. (See accompanying file LICENSE.txt)
 /// \briefFile{Extensions built on top of \refBoost{Boost.Operator,utility/operators.htm}.}
-#ifndef CPPQEDCORE_UTILS_OPERATORS_H_INCLUDED
-#define CPPQEDCORE_UTILS_OPERATORS_H_INCLUDED
+#pragma once
 
 #include "ComplexExtensions.h"
+
+#include <Eigen/Dense>
 
 #include <boost/operators.hpp>
 
 
 namespace linalg {
 
-namespace details { struct EmptyBase {}; }
+
+using CMatrix=Eigen::MatrixX<dcomp>;
+using CVector=Eigen::VectorX<dcomp>;
+
 
 /// Operator aggregate for a complex vector space built on top of \refBoost{Boost.Operator,utility/operators.htm}.
 /**
@@ -20,15 +24,15 @@ namespace details { struct EmptyBase {}; }
  * 
  * \tparam T the vector type
  */
-template<typename T, typename B=details::EmptyBase>
+template<typename T, typename Base = std::nullopt_t>
 struct VectorSpace
   : boost::additive1     <T,        // Abel group
     boost::multiplicative<T,double, // Vector space
     boost::multiplicative<T,dcomp,  // "
     /*  boost::multipliable1  <T,        // Direct product */
-    boost::equality_comparable<T> > > > {};
+    boost::equality_comparable<T> > > >, Base {};
 
 
 } // linalg
 
-#endif // CPPQEDCORE_UTILS_OPERATORS_H_INCLUDED
+
