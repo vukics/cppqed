043418e95c102db27968a64d53830651c776c0d3
András Vukics <andras.vukics@gmail.com>
2022-11-08 09:52:32 +0100

    Hamiltonian exemplifies functional architecture of fundamental structural elements

--- CPPQEDcore/structure/Hamiltonian.h
+++ CPPQEDcore/structure/Hamiltonian.h
@@ -4,7 +4,7 @@
 
 #include "StateVector.h"
 
-#include "Overoaded.h"
+#include "Overloaded.h"
 
 #include <variant>
 
@@ -13,38 +13,46 @@ namespace structure {
 
 
 template <size_t RANK>
-using TimeIndependentContribution = std::function<void(StateVectorConstView<RANK> psi, StateVectorView<RANK> dpsidt)>;
+using TimeIndependentTerm = std::function<void(StateVectorConstView<RANK> psi, StateVectorView<RANK> dpsidt)>;
 
 
 template <size_t RANK>
-using OneTimeDependentContribution = std::function<void(double t, StateVectorConstView<RANK> psi, StateVectorView<RANK> dpsidt)>;
+using OneTimeDependentTerm = std::function<void(double t, StateVectorConstView<RANK> psi, StateVectorView<RANK> dpsidt)>;
 
 
 template <size_t RANK>
-using TwoTimeDependentContribution = std::function<void(double t, StateVectorConstView<RANK> psi, StateVectorView<RANK> dpsidt, double t0)>;
+using TwoTimeDependentTerm = std::function<void(double t, StateVectorConstView<RANK> psi, StateVectorView<RANK> dpsidt, double t0)>;
 
 
+/// a label and a term
+/*
+ * the `label` becomes a `prefix:label` when the system becomes part of a more complex system
+ */
 template <size_t RANK>
-using HamiltonianContribution = std::variant<TimeIndependentContribution<RANK>,OneTimeDependentContribution<RANK>,TwoTimeDependentContribution<RANK>>;
+using HamiltonianTerm = std::tuple<std::string,std::variant<TimeIndependentTerm<RANK>,OneTimeDependentTerm<RANK>,TwoTimeDependentTerm<RANK>>>;
 
 
-template <size_t RANK>
-using Hamiltonian = std::list<HamiltonianContribution<RANK>>;
+// e.g. `std::list<HamiltonianTerm<RANK>>`, but transformed views might be necessary
+template <typename T, size_t RANK>
+concept hamiltonian = 
+  std::ranges::forward_range<T> && 
+  std::is_same_v<std::ranges::range_value_t<T>,
+                 HamiltonianTerm<RANK>>;
 
 
-template <typename H, size_t RANK>
-concept HamiltonianSystem = std::convertible_to<H,HamiltonianContribution<RANK>>;
-
-
-template <size_t RANK>// requires Hamiltonian<H,RANK>
-void addContribution(const Hamiltonian<RANK>& hamiltonian, double t, StateVectorConstView<RANK> psi, StateVectorView<RANK> dpsidt, double t0)
+/// applying a Hamiltonian is by default interpreted as |dpsidt>+=H|psi>
+/**
+ * However, if indexing is done carefully, `psi` and `dpsidt` can refer to the same underlying data
+ */
+template <size_t RANK, hamiltonian<RANK> HA>
+void apply(const HA& ha, double t, StateVectorConstView<RANK> psi, StateVectorView<RANK> dpsidt, double t0)
 {
-  for (auto& contribution : hamiltonian)
+  for (auto& term : ha)
     std::visit(cppqedutils::overload{
-      [&] (const TimeIndependentContribution<RANK>& c) {c(psi,dpsidt);},
-      [&] (const OneTimeDependentContribution<RANK>& c) {c(t-t0,psi,dpsidt);},
-      [&] (const TwoTimeDependentContribution<RANK>& c) {c(t,psi,dpsidt,t0);},
-    },contribution);
+      [&] (const TimeIndependentTerm<RANK>& c) {c(psi,dpsidt);},
+      [&] (const OneTimeDependentTerm<RANK>& c) {c(t-t0,psi,dpsidt);},
+      [&] (const TwoTimeDependentTerm<RANK>& c) {c(t,psi,dpsidt,t0);},
+    },std::get<1>(term));
 }
 
 
@@ -53,7 +61,7 @@ void addContribution(const Hamiltonian<RANK>& hamiltonian, double t, StateVector
 
 /*
 template <typename SYSTEM, size_t RANK>
-void addContribution(SYSTEM&& sys, double t, StateVectorConstView<RANK> psi, StateVectorView<RANK> dpsidt, double t0)
+void apply(SYSTEM&& sys, double t, StateVectorConstView<RANK> psi, StateVectorView<RANK> dpsidt, double t0)
 {
   if constexpr (std::is_convertible_v<SYSTEM,Hamiltonian<RANK>>) addContribution(std::static_cast<const >)
 }
