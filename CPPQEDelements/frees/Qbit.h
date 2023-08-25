7d9763fbbafacdd662ed51bb8c7557f613c583d9
András Vukics <andras.vukics@gmail.com>
2023-08-25 11:20:20 +0200

    maker + Pars for Qbit

--- CPPQEDelements/frees/Qbit.h
+++ CPPQEDelements/frees/Qbit.h
@@ -2,9 +2,9 @@
 /// \briefFile{Defines the qbit-bundle (tackling the dynamics of a single qbit)}
 #pragma once
 
-// #include "ParsQbit.h"
-
 #include "QuantumSystemDynamics.h"
+
+#include "Pars.h"
 
 
 namespace qbit {
@@ -40,24 +40,62 @@ Lindblad<1> gain(double gamma_p) {return {"gain", sigmaPlusJump(gamma_p), sigmaP
 
 static constexpr auto expectationValues = [] (lazy_density_operator<1> auto rho) { return hana::make_tuple(_(rho,0),_(rho,0,1)); };
 
-::cppqedutils::LogTree label(decltype(expectationValues)) { return {"population","polarization"}; }
+::cppqedutils::LogTree label(decltype(expectationValues)) { return {"population ground","polarization"}; }
 
 
 StateVector<1> state0();// {return mode::fock(0,2);}
 StateVector<1> state1();// {return mode::fock(1,2);}
 StateVector<1> init(dcomp psi1);
 
-/*
-auto make(dcomp zSch, dcomp zI, dcomp eta, double gamma_m, double gamma_p, double gamma_phi)
+
+auto make(dcomp zSch/*, dcomp zI*/, dcomp eta, double gamma_m, double gamma_p/*, double gamma_phi*/)
 {
+  std::vector<Lindblad<1>> liouvillian;
+
+  if (gamma_m) liouvillian.push_back(loss(gamma_m));
+  if (gamma_p) liouvillian.push_back(gain(gamma_m));
+  // if (gamma_phi) liouvillian.emplace(dephasing(gamma_phi));
+
   return QuantumSystemDynamics{
-    {{"zSch",zSch,1},{"zI",zI,1},{"η",eta,1},{"γ_m",gamma_m,1},{"γ_p",gamma_p,1},{"γ_phi",gamma_phi,1}},
-    {{"diagSch",diagonalH(zSch)},{"diagI",propagator(zI)},{"drive",offDiagonalH(eta)}},
-    {loss(gamma_m),gain(gamma_p),dephasing(gamma_phi)},
+    std::vector<SystemFrequencyDescriptor>{{"zSch",zSch,1}/*,{"zI",zI,1}*/,{"η",eta,1},{"γ_m",gamma_m,1},{"γ_p",gamma_p,1}/*,{"γ_phi",gamma_phi,1}*/},
+    liouvillian,
+    makeHamiltonianCollection<1>(diagonalH(zSch)/*,{"diagI",propagator(zI)}*/,offDiagonalH(eta)),
     expectationValues
   };
 }
-*/
+
+
+auto make(double delta, dcomp eta, double gamma_m, double gamma_p)
+{
+  return make(dcomp{gamma_m-gamma_p,-delta},eta,gamma_m,gamma_p);
+}
+
+
+
+struct Pars
+{
+  double delta, gamma_m, gamma_p;
+  dcomp eta;
+
+  Pars(popl::OptionParser& op, std::string mod="")
+  {
+    using ::parameters::_;
+    addTuple(mod,op,"Qubit",
+      _("delta","qubit detuning",-10.,delta),
+//      _("init","initial condition",dcomp(0.),init),
+      _("eta","qubit drive",0.,eta),
+      _("gamma_m","qubit decay rate",10.,gamma_m),
+      _("gamma_p","qubit gain rate",10.,gamma_p));
+  }
+
+};
+
+
+auto make(const Pars& p)
+{
+  return make(p.delta,p.eta,p.gamma_m,p.gamma_p);
+}
+
 
 } // qbit
 
