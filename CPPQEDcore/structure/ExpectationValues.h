1caa539b0419771ed1c32c87fb1a427a4a834ce5
András Vukics <andras.vukics@gmail.com>
2024-07-20 15:51:52 +0200

    expectation_values can now also be hana::tuple’s of functionals similarly to hamiltonian’s

--- CPPQEDcore/structure/ExpectationValues.h
+++ CPPQEDcore/structure/ExpectationValues.h
@@ -43,45 +43,29 @@ requires (DensityOperatorConstView<RANK> rho) { { l(rho) } -> temporal_data_poin
 template <typename L, size_t RANK>
 concept functional = time_dependent_functional<L,RANK> || time_independent_functional<L,RANK> ;
 
+template <typename H, size_t RANK> constexpr bool recursiveTrait = [] {
+  if constexpr (hana_sequence<H>)
+    return !!hana::all_of(
+      decltype(hana::transform(std::declval<H>(), hana::typeid_)){},
+      [] <class T> (T) { return recursiveTrait<typename T::type,RANK>; } );
+  else return functional<H,RANK> ;
+} ();
 
 } // expectation_values_ns
 
 
-/// pre- & postcondition: the LogTree must have the same structure as the temporal_data_point returned by expectation_value
 template <typename L, size_t RANK>
-concept expectation_values = /* labelled<L> && */ expectation_values_ns::functional<L,RANK> ;
-
-
-template <size_t RANK>
-void checkConsistencyWithLabels (const expectation_values<RANK> auto&);
+concept expectation_values = expectation_values_ns::recursiveTrait<L,RANK> ;
 
 
 template <size_t RANK, expectation_values<RANK> EV>
 auto calculateExpectationValues(const EV& ev, double t, lazy_density_operator<RANK> auto matrix)
 {
-  if constexpr (expectation_values_ns::time_dependent_functional<EV,RANK>) return ev(t,matrix);
-  else return ev(matrix);
+  if      constexpr (hana_sequence<EV>) return hana::transform(ev, [&] (const auto& f) {return calculateExpectationValues<RANK>(f,t,matrix);}) ;
+  else if constexpr (expectation_values_ns::time_dependent_functional<EV,RANK>) return ev(t,matrix);
+  else if constexpr (expectation_values_ns::time_independent_functional<EV,RANK>) return ev(matrix);
+  else static_assert(always_false<EV>::value, "Unsupported type in calculateExpectationValues");
 }
-
-
-
-/* Another definition of key_labels that mirrors that of temporal_data_points is probably an overkill
-/// hana::tuple of std::string or such hana::tuples (recursive definition)
-namespace expectation_values_ns {
-
-template <typename T> constexpr bool kl = false;
-
-template <> constexpr bool kl<std::string> = true;
-
-template <hana_sequence S> constexpr bool kl<S> = !!hana::all_of(
-  decltype(hana::transform(std::declval<S>(), hana::typeid_)){},
-  []<class T>(T) { return kl<typename T::type>; });
-
-} // expectation_values_ns
-
-template <typename T> concept key_labels = expectation_values_ns::kl<T>;
-*/
-
 
 
 } // structure
